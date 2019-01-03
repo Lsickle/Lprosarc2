f01cb583f3894155c39ceb3647380fe550d21d6e
sistemas prosarc <davidboy69@hotmail.com>
2019-01-03 13:28:44 -0500

    npm install git://github.com/mstratman/jQuery-Smart-Wizard.git --save-dev

--- resources/views/declaraciones/show.blade.php
+++ resources/views/declaraciones/show.blade.php
@@ -77,9 +77,9 @@
     <div class="col-md-9">
       <div class="nav-tabs-custom">
         <ul class="nav nav-tabs">
-          <li><a href="#ClienteFicha" data-toggle="tab">Cliente</a></li>
+          <li class="active"><a href="#ClienteFicha" data-toggle="tab">Cliente</a></li>
           <li><a href="#activity" data-toggle="tab">Generador</a></li>
-          <li class="active"><a href="#Residuos" data-toggle="tab">Residuos</a></li>
+          <li><a href="#Residuos" data-toggle="tab">Residuos</a></li>
           <li><a href="#settings" data-toggle="tab">Hojas de seguridad</a></li>
           <li><a href="#settings" data-toggle="tab">Tarjeta de emergencia</a></li>
         </ul>
@@ -107,7 +107,7 @@
             <!-- /.Ficha del cliente -->
             
           </div>
-          <!-- /.tab-pane -->
+          <!-- tab-pane -->
           <div class="tab-pane" id="Residuos">
             <div class="row">
               <div class="col-md-12 col-md-offset-0">
@@ -117,8 +117,32 @@
                     <h3 class="box-title">{{ trans('adminlte_lang::LangRespel.Respellist') }}</h3>
                   </div>
                   <div>
-                    <button class="btn btn-primary" id="addRow">Nuevo Residuo</button>
+                    <button class="btn btn-primary" id="addRow"><a href="#addRowWizz" data-toggle="tab">Nuevo Residuo</a></button>
                   </div>
+                  {{-- <div _ngcontent-c56="" class="col-md-12 col-lg-6 col-xxxl-3">
+                    <nb-card _ngcontent-c56="" class="form-input-card" _nghost-c26="">
+                      <nb-card-header _ngcontent-c56="">
+                        Return Result From Dialog
+                      </nb-card-header>
+                      <nb-card-body _ngcontent-c56="" class="result-from-dialog">
+                        <button _ngcontent-c56="" nbbutton="" _nghost-c57="" tabindex="0">
+                          Enter Name
+                        </button>
+                        <br _ngcontent-c56="">
+                        <h3 _ngcontent-c56="" class="title">
+                          Names:
+                        </h3>
+                        <ul _ngcontent-c56=""><!---->
+                          <li _ngcontent-c56="" class="ng-star-inserted">
+                            luis
+                          </li>
+                          <li _ngcontent-c56="" class="ng-star-inserted">
+                            4123
+                          </li>
+                        </ul>
+                      </nb-card-body>
+                    </nb-card>
+                  </div> --}}
                   <!-- /.box-header -->
                   <div class="box-body">
                     <table id="RespelTable" class="table table-bordered table-striped" style="white-space:nowrap; width:100%">
@@ -195,6 +219,136 @@
             </div>
           </div>
           <!-- /.tab-pane -->
+          <!-- tab-pane -->
+          <div class="tab-pane" id="addRowWizz">
+            <p>This is a basic form wizard example that inherits the colors from the selected scheme.</p>
+                    <div id="wizard" class="form_wizard wizard_horizontal">
+                      <ul class="wizard_steps">
+                        <li>
+                          <a href="#step-1">
+                            <span class="step_no">1</span>
+                            <span class="step_descr">
+                                              Step 1<br />
+                                              <small>Step 1 description</small>
+                                          </span>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#step-2">
+                            <span class="step_no">2</span>
+                            <span class="step_descr">
+                                              Step 2<br />
+                                              <small>Step 2 description</small>
+                                          </span>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#step-3">
+                            <span class="step_no">3</span>
+                            <span class="step_descr">
+                                              Step 3<br />
+                                              <small>Step 3 description</small>
+                                          </span>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#step-4">
+                            <span class="step_no">4</span>
+                            <span class="step_descr">
+                                              Step 4<br />
+                                              <small>Step 4 description</small>
+                                          </span>
+                          </a>
+                        </li>
+                      </ul>
+                      <div id="step-1">
+                        <form class="form-horizontal form-label-left">
+
+                          <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">First Name <span class="required">*</span>
+                            </label>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                              <input type="text" id="first-name" required="required" class="form-control col-md-7 col-xs-12">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Last Name <span class="required">*</span>
+                            </label>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                              <input type="text" id="last-name" name="last-name" required="required" class="form-control col-md-7 col-xs-12">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Middle Name / Initial</label>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                              <input id="middle-name" class="form-control col-md-7 col-xs-12" type="text" name="middle-name">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12">Gender</label>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                              <div id="gender" class="btn-group" data-toggle="buttons">
+                                <label class="btn btn-default" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                                  <input type="radio" name="gender" value="male"> &nbsp; Male &nbsp;
+                                </label>
+                                <label class="btn btn-primary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                                  <input type="radio" name="gender" value="female"> Female
+                                </label>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12">Date Of Birth <span class="required">*</span>
+                            </label>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                              <input id="birthday" class="date-picker form-control col-md-7 col-xs-12" required="required" type="text">
+                            </div>
+                          </div>
+
+                        </form>
+
+                      </div>
+                      <div id="step-2">
+                        <h2 class="StepTitle">Step 2 Content</h2>
+                        <p>
+                          do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                          in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                      </div>
+                      <div id="step-3">
+                        <h2 class="StepTitle">Step 3 Content</h2>
+                        <p>
+                          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+                          eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                          in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                      </div>
+                      <div id="step-4">
+                        <h2 class="StepTitle">Step 4 Content</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                          in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                          in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                      </div>
+
+                    </div>
+          </div>
+          <!-- /.tab-pane -->
+          <!-- tab-pane -->
           <div class="tab-pane" id="settings">
             <form class="form-horizontal">
               <div class="form-group">
