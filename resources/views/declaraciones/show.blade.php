b3bb1cfd80c6d9f175f7824689479b9113649bd2
sistemas prosarc <davidboy69@hotmail.com>
2019-01-08 10:35:58 -0500

    se añade componente vue para requerimientos de residuos

--- resources/views/declaraciones/show.blade.php
+++ resources/views/declaraciones/show.blade.php
@@ -235,31 +235,7 @@
                 Step Content
               </div>
               <div id="step-2" class="">
-                <div class="form-group">
-                  <label class="control-label col-md-3 col-sm-3 col-xs-12">Switch</label>
-                  <div class="col-md-9 col-sm-9 col-xs-12">
-                    <div class="">
-                      <label>
-                        <input type="checkbox" class="testswitch"/> Checked
-                      </label>
-                    </div>
-                    <div class="">
-                      <label>
-                        <input type="checkbox" class="testswitch"/> Unchecked
-                      </label>
-                    </div>
-                    <div class="">
-                      <label>
-                        <input type="checkbox" class="testswitch"/> Disabled
-                      </label>
-                    </div>
-                    <div class="">
-                      <label>
-                        <input type="checkbox" class="testswitch"/> Disabled Checked
-                      </label>
-                    </div>
-                  </div>
-                </div>
+                <requirements-component></requirements-component>
               </div>
               <div id="step-3" class="">
                 Step Content
