8c76680b9d1d28406cc028bf5104b8668d8b141d
Luis De la hoz <luisdelahoz0@gmail.com>
2019-01-08 21:02:14 -0500

    se añade logica de validacion de toles de cada usuario

--- app/User.php
+++ app/User.php
@@ -10,6 +10,40 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    public function roles(){
+        return $this->belongsToMany('App\Role');
+    }
+
+    public function authorizeRoles($roles){
+        if ($this->hasAnyRole($roles)) {
+            return true;
+        }
+        abort(401, 'Esta accion no esta autorizada');
+    }
+
+    public function hasAnyRole($roles){
+        if (is_array($roles)) {
+            foreach ($roles as $role) {
+                if ($this->hasRole($role)) {
+                return true;
+                }
+            }
+        } else {
+            if ($this->hasRole($roles)) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public function hasRole($role){
+        if ($this->roles()->where('name',$role)->first()) {
+            return true;
+        }
+        return false;
+    }
+    
     /**
      * The attributes that are mass assignable.
      *
