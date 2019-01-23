60300baacdcf8c6fdb2e4eb7bb6c2fb52113b111
luis de la hoz <davidboy69@hotmail.com>
2019-01-23 14:53:12 -0500

    se añade funcionalidad para redireccionar al usuario en caso de que tenga deshabilitado javascript en su navegador

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ Route::group(['middleware' => 'auth'], function () {
     //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
     #adminlte_routes
 });
+Route::get('/noscriptpage', function () {
+    return view('noscriptpage');
+});
 Route::resource('/clientes', 'clientcontoller');
 Route::resource('/sclientes', 'sclientcontroller');
 Route::resource('/Generadores', 'genercontroller');
