8f3fa72d42ec82bf5839c6a96a8a6aff2a9e47c8
luis de la hoz <davidboy69@hotmail.com>
2019-02-04 09:29:59 -0500

    migration para crear la tabla de autoria de base de datos lprosarc

--- database/migrations/2019_02_04_091547_create_audits_table.php
+++ database/migrations/2019_02_04_091547_create_audits_table.php
@@ -16,6 +16,13 @@ class CreateAuditsTable extends Migration
         Schema::create('audits', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+            $table->string('AuditTabla');
+            $table->string('AuditRegistro');
+            $table->string('AuditUser');
+            $table->string('Auditlog'); /*coniene toda la informacion del request con el que solicita el update, si el request tiene archivos tambien viene el nombre del archivo nuevo*/
+
+
+
         });
     }
 
