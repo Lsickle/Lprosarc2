59614f7024d6bb9d8cf7ab4c896e824c4aa90482
Andres <andres13marmon@gmail.com>
2019-02-15 11:33:14 -0500

    Modificando la vista de departamento y municipio

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -459,6 +459,40 @@ $(document).ready(function(){
     });
   });
 </script>
+<script>
+    $(document).ready(function() {
+      $('#departamentTable').DataTable( {
+        "scrollX": false,
+        "autoWidth": true,
+        "keys": true,
+        "responsive": true
+        // "columnDefs": [ {
+        //   "targets": 5,
+        //   "data": "id",
+        //   "render": function ( data, type, row, meta ) {
+        //     return "<button type='button' class='btn btn-info btn-lg' data-toggle='modal' data-target="+ data +"Modal>Ver</button>";
+        //   }
+        // }]
+      });
+    });
+  </script>
+  <script>
+      $(document).ready(function() {
+        $('#municipalityTable').DataTable( {
+          "scrollX": false,
+          "autoWidth": true,
+          "keys": true,
+          "responsive": true
+          // "columnDefs": [ {
+          //   "targets": 5,
+          //   "data": "id",
+          //   "render": function ( data, type, row, meta ) {
+          //     return "<button type='button' class='btn btn-info btn-lg' data-toggle='modal' data-target="+ data +"Modal>Ver</button>";
+          //   }
+          // }]
+        });
+      });
+    </script>
   
 
 
