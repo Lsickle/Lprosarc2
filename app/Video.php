64430c8e66c467e4265d1a88d3fdeffe729f3daf
luis de la hoz <davidoy69@hotmail.com>
2019-02-18 09:41:47 -0500

    + modelo y migracion para la tabla video

---
+++ app/Video.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Video extends Model
+{
+    protected $table = 'videos';
+
+    protected $table = ['VideoName', 'VideoTipo', 'VideoTipoOther','VideoTipoOther', 'VideoSrc' 'VideoFormat'];
+
+    public $primaryKey = 'ID_Video';
+}
