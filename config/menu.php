049d3cb586604b88b887a83cbbbd575c1b1e731b
Duvan <programmerloquillo@gmail.com>
2019-03-07 11:21:04 -0500

    Terminando el SideBar

--- config/menu.php
+++ config/menu.php
@@ -10,6 +10,7 @@ Menu::macro('adminlteMenu', function () {
     return Menu::new()
         ->addClass('sidebar-menu tree')->setAttribute('data-widget','tree');
 });
+
 Menu::macro('adminlteSeparator', function ($title) {
     return Html::raw($title)->addParentClass('header');
 });
@@ -21,7 +22,7 @@ Menu::macro('sidebar', function () {//COMIENZO DEL SIDEBAR EN VERSION DE MENU
         ->add(Html::raw(trans('adminlte_lang::message.header'))->addParentClass('header'))
 
         /*PESTAÑA DE INICIO / HOME*/
-        ->action('HomeController@index', '<i class="fa fa-home"></i><span>'.trans('adminlte_lang::message.home').'</span>')
+        ->action('HomeController@index', '<i class="fa fa-home"></i>'.trans('adminlte_lang::message.home').'')
 
         /*VALIDACION DEL ROL CON EL QUE INGRESO*/
          /*->addIf(Auth::user()->UsRol == "Programador" || Auth::user()->UsRol == "admin")*/
@@ -29,7 +30,7 @@ Menu::macro('sidebar', function () {//COMIENZO DEL SIDEBAR EN VERSION DE MENU
         /*PESTAÑA DE CLIENTES*/
         ->add(Menu::new()
                 /*"PREPEND SIRVE PARA COLOCAR UN ETIQUETA FUERA DEL MENU (ul)"*/
-            ->prepend('<a href="#"><i class="fa fa-id-card"></i><span>'.trans('adminlte_lang::message.contacts').'</span><i class="fas fa-angle-left pull-right"></i></a>')
+            ->prepend('<a href="#"><i class="fa fa-id-card"></i>'.trans('adminlte_lang::message.contacts').'<i class="fas fa-angle-left pull-right"></i></a>')
 
              /*LE ASIGNA UNA CLASE A ESA ETIQUETA CUANDO ¡NO! ESTA ACTIVA*/
             ->addParentClass('treeview')
@@ -42,7 +43,7 @@ Menu::macro('sidebar', function () {//COMIENZO DEL SIDEBAR EN VERSION DE MENU
 
             /*UN SUBMENU */
             ->add(Menu::new()
-                ->prepend('<a href="#"><i class="fa fa-building"></i> Sedes<span class="pull-right-container"><i class="fas fa-angle-left pull-right"></i></span></a>')
+                ->prepend('<a href="#"><i class="fa fa-building"></i> Sedes<span class="pull-right-container"><i class="fas fa-angle-left pull-right"></i></a>')
                 ->addParentClass('treeview')
                 ->add(Link::toUrl('/sclientes', '<i class="fa fa-map"></i>'. trans('adminlte_lang::message.csedeindex')))
                 ->addClass('treeview-menu')
@@ -50,12 +51,12 @@ Menu::macro('sidebar', function () {//COMIENZO DEL SIDEBAR EN VERSION DE MENU
         )
         /*PESTAÑA DE GENERADORES*/
         ->add(Menu::new()
-            ->prepend('<a href="#"><i class="fa fa-industry"></i> <span>'. trans('adminlte_lang::message.genermenu').'</span> <i class="fas fa-angle-left pull-right"></i></a>')
+            ->prepend('<a href="#"><i class="fa fa-industry"></i> '. trans('adminlte_lang::message.genermenu').' <i class="fas fa-angle-left pull-right"></i></a>')
             ->addParentClass('treeview')
             ->add(Link::toUrl('/generadores', '<i class="fa fa-list-ul"></i>'. trans('adminlte_lang::message.generindex')))
             ->addClass('treeview-menu')
             ->add(Menu::new()
-                ->prepend('<a href="#"><i class="fa fa-building"></i>Sede Generador<span class="pull-right-container"><i class="fas fa-angle-left pull-right"></i> </span></a>')
+                ->prepend('<a href="#"><i class="fa fa-building"></i>Sede Generador<span class="pull-right-container"><i class="fas fa-angle-left pull-right"></i> </a>')
                 ->addParentClass('treeview')
                 ->add(Link::toUrl('/sgeneradores', '<i class="fa fa-map"></i>'. trans('adminlte_lang::message.csedeindex')))
                 ->addClass('treeview-menu')
@@ -63,7 +64,7 @@ Menu::macro('sidebar', function () {//COMIENZO DEL SIDEBAR EN VERSION DE MENU
         )
         /*PESTAÑA DE RESIDUOS*/
         ->add(Menu::new()
-            ->prepend('<a href="#"><i class="fas fa-biohazard"></i> <span>'. trans('adminlte_lang::langresiduos.residuolisttitle').'</span> <i class="fas fa-angle-left pull-right"></i></a>')
+            ->prepend('<a href="#"><i class="fas fa-biohazard"></i> '. trans('adminlte_lang::langresiduos.residuolisttitle').' <i class="fas fa-angle-left pull-right"></i></a>')
             ->addParentClass('treeview')
             ->add(Link::toUrl('/respels', '<i class="fa fa-search"></i>'. trans('adminlte_lang::langresiduos.residuolisttitle')))
             ->add(Link::toUrl('/requerimientos', '<i class="fas fa-list-ol"></i> Requerimientos de Residuos'))
@@ -72,7 +73,7 @@ Menu::macro('sidebar', function () {//COMIENZO DEL SIDEBAR EN VERSION DE MENU
         )
         /*PESTAÑA DE DOCUMENTOS*/
         ->add(Menu::new()
-            ->prepend('<a href="#"><i class="fas fa-print"></i> <span>Documentos</span> <i class="fas fa-angle-left pull-right"></i></a>')
+            ->prepend('<a href="#"><i class="fas fa-print"></i> Documentos <i class="fas fa-angle-left pull-right"></i></a>')
             ->addParentClass('treeview')
             ->add(Link::toUrl('/certificado', '<i class="fas fa-certificate"></i> Certificados'))
             ->add(Link::toUrl('/manifiesto', '<i class="fas fa-tools"></i> Manifiestos'))
@@ -80,10 +81,11 @@ Menu::macro('sidebar', function () {//COMIENZO DEL SIDEBAR EN VERSION DE MENU
         )
         /*PESTAÑA DE PERSONAL*/
         ->add(Menu::new()
-            ->prepend('<a href="#"><i class="fas fa-users"></i> <span>Personal</span> <i class="fas fa-angle-left pull-right"></i></a>')
+            ->prepend('<a href="#"><i class="fas fa-users"></i> Personal <i class="fas fa-angle-left pull-right"></i></a>')
             ->addParentClass('treeview')
             ->add(Link::toUrl('/personal', '<i class="fas fa-list-alt"></i> Listar'))
             ->add(Link::toUrl('/asistencia', '<i class="fas fa-tasks"></i> Asistencia'))
+            ->add(Link::toUrl('/horario', '<i class="fas fa-user-clock"></i> Horario'))
             ->add(Link::toUrl('/cargos', '<i class="fas fa-tools"></i> Cargos'))
             ->add(Link::toUrl('/areas', '<i class="fas fa-archive"></i> Areas'))
             ->add(Link::toUrl('/inventariotech', '<i class="fas fa-laptop"></i> Inventario Tecnologia'))
@@ -91,7 +93,7 @@ Menu::macro('sidebar', function () {//COMIENZO DEL SIDEBAR EN VERSION DE MENU
         )
         /*PESTAÑA DE CAPACITACIONES*/
         ->add(Menu::new()
-            ->prepend('<a href="#"><i class="fas fa-scroll"></i> <span>Capacitaciones</span> <i class="fas fa-angle-left pull-right"></i></a>')
+            ->prepend('<a href="#"><i class="fas fa-scroll"></i> Capacitaciones <i class="fas fa-angle-left pull-right"></i></a>')
             ->addParentClass('treeview')
             ->add(Link::toUrl('/capacitacion', '<i class="fas fa-list-alt"></i> Listar'))
             ->add(Link::toUrl('/capacitacion-personal', '<i class="fas fa-user-check"></i> Capacitaciones del personal'))
@@ -99,106 +101,43 @@ Menu::macro('sidebar', function () {//COMIENZO DEL SIDEBAR EN VERSION DE MENU
         )
         /*PESTAÑA DE VEHICULOS*/
         ->add(Menu::new()
-            ->prepend('<a href="#"><i class="fas fa-truck-moving"></i></i> <span> Vehiculos</span> <i class="fas fa-angle-left pull-right"></i></a>')
+            ->prepend('<a href="#"><i class="fas fa-truck-moving"></i></i>  Vehiculos <i class="fas fa-angle-left pull-right"></i></a>')
             ->addParentClass('treeview')
             ->add(Link::toUrl('/vehicle', '<i class="fas fa-list-alt"></i> Listar'))
             ->add(Link::toUrl('/vehicle-programacion', '<i class="fas fa-calendar-alt"></i> Programación'))
             ->add(Link::toUrl('/vehicle-mantenimiento', '<i class="fas fa-tools"></i> Mantenimiento'))
             ->addClass('treeview-menu')
         )
-        /*PESTAÑA DE VEHICULOS*/
+        /*PESTAÑA DE COMPRA*/
         ->add(Menu::new()
-            ->prepend('<a href="#"><i class="fas fa-truck-moving"></i></i> <span> Vehiculos</span> <i class="fas fa-angle-left pull-right"></i></a>')
+            ->prepend('<a href="#"><i class="fas fa-money-bill-wave"></i></i>  Compra <i class="fa fa-angle-left pull-right"></i></a>')
             ->addParentClass('treeview')
-            ->add(Link::toUrl('/vehicle', '<i class="fas fa-list-alt"></i> Listar'))
-            ->add(Link::toUrl('/vehicle-programacion', '<i class="fas fa-calendar-alt"></i> Programación'))
-            ->add(Link::toUrl('/vehicle-mantenimiento', '<i class="fas fa-tools"></i> Mantenimiento'))
+            ->add(Link::toUrl('/compra/orden', '<i class="fas fa-file-invoice-dollar"></i> Orden'))
+            ->add(Link::toUrl('/compra/cotizacion', '<i class="fas fa-file-invoice"></i> Cotizacion'))
             ->addClass('treeview-menu')
         )
-        /*PESTAÑA DE VEHICULOS*/
+        /*PESTAÑA DE ACTIVOS*/
         ->add(Menu::new()
-            ->prepend('<a href="#"><i class="fas fa-truck-moving"></i></i> <span> Vehiculos</span> <i class="fas fa-angle-left pull-right"></i></a>')
+            ->prepend('<a href="#"><i class="fas fa-laptop"></i> Activos <i class="fa fa-angle-left pull-right"></i></a>')
             ->addParentClass('treeview')
-            ->add(Link::toUrl('/vehicle', '<i class="fas fa-list-alt"></i> Listar'))
-            ->add(Link::toUrl('/vehicle-programacion', '<i class="fas fa-calendar-alt"></i> Programación'))
-            ->add(Link::toUrl('/vehicle-mantenimiento', '<i class="fas fa-tools"></i> Mantenimiento'))
+            ->add(Link::toUrl('/activos', '<i class="fas fa-list-alt"></i> Inventario'))
+            ->add(Link::toUrl('/activos-movimiento', '<i class="fas fa-list-alt"></i> Movimientos'))
             ->addClass('treeview-menu')
         )
-        /*PESTAÑA DE VEHICULOS*/
+        /*PESTAÑA DE SOLICITUD*/
         ->add(Menu::new()
-            ->prepend('<a href="#"><i class="fas fa-truck-moving"></i></i> <span> Vehiculos</span> <i class="fas fa-angle-left pull-right"></i></a>')
+            ->prepend('<a href="#"><i class="fas fa-people-carry"></i> Solicitud<i class="fa fa-angle-left pull-right"></i></a>')
             ->addParentClass('treeview')
-            ->add(Link::toUrl('/vehicle', '<i class="fas fa-list-alt"></i> Listar'))
-            ->add(Link::toUrl('/vehicle-programacion', '<i class="fas fa-calendar-alt"></i> Programación'))
-            ->add(Link::toUrl('/vehicle-mantenimiento', '<i class="fas fa-tools"></i> Mantenimiento'))
+            ->add(Link::toUrl('/solicitud-residuo', '<i class="fas fa-list-alt"></i>  Inventario de Residuos'))
+            ->add(Link::toUrl('/solicitud-servicio', '<i class="fas fa-file-signature"></i> Inventario de servicios'))
+            ->add(Link::toUrl('/recurso', '<i class="fas fa-video"></i> Recursos'))
             ->addClass('treeview-menu')
         )
-        /*PESTAÑA DE VEHICULOS*/
-        ->add(Menu::new()
-            ->prepend('<a href="#"><i class="fas fa-truck-moving"></i></i> <span> Vehiculos</span> <i class="fas fa-angle-left pull-right"></i></a>')
-            ->addParentClass('treeview')
-            ->add(Link::toUrl('/vehicle', '<i class="fas fa-list-alt"></i> Listar'))
-            ->add(Link::toUrl('/vehicle-programacion', '<i class="fas fa-calendar-alt"></i> Programación'))
-            ->add(Link::toUrl('/vehicle-mantenimiento', '<i class="fas fa-tools"></i> Mantenimiento'))
-            ->addClass('treeview-menu')
-        )
-        /*PESTAÑA DE VEHICULOS*/
-        ->add(Menu::new()
-            ->prepend('<a href="#"><i class="fas fa-truck-moving"></i></i> <span> Vehiculos</span> <i class="fas fa-angle-left pull-right"></i></a>')
-            ->addParentClass('treeview')
-            ->add(Link::toUrl('/vehicle', '<i class="fas fa-list-alt"></i> Listar'))
-            ->add(Link::toUrl('/vehicle-programacion', '<i class="fas fa-calendar-alt"></i> Programación'))
-            ->add(Link::toUrl('/vehicle-mantenimiento', '<i class="fas fa-tools"></i> Mantenimiento'))
-            ->addClass('treeview-menu')
-        )
+        /*PESTAÑA DE ARTICULOS*/
+        ->add(Link::toUrl('/articulos-proveedor', '<i class="far fa-newspaper"></i> Articulos'))
 
+        /*PESTAÑA DE CODIGO QR*/
+        ->add(Link::toUrl('/code', '<i class="fas fa-qrcode"></i> Codigo Qr'))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        ->add(Menu::adminlteSeparator('Acacha Adminlte'))
-        #adminlte_menu
-        /*->add(Link::toUrl('/prueba', '<i class="fa fa-link"></i><span>/prueba</span>'))*/
-        ->add(Menu::adminlteSeparator('SECONDARY MENU'))
-        ->add(Menu::new()->prepend('<a href="#"><i class="fa fa-share"></i><span>Multilevel</span> <i class="fas fa-angle-left pull-right"></i></a>')
-            ->addParentClass('treeview')
-            ->add(Link::to('/prueba', 'Link1'))->addClass('treeview-menu')
-            ->add(Link::to('/clientes', 'Link2'))
-            ->url('/clientes/create', 'Google')
-            ->add(Menu::new()->prepend('<a href="#"><span>Multilevel 2</span> <i class="fas fa-angle-left pull-right"></i></a>')
-                ->addParentClass('treeview')
-                ->add(Link::to('/link21', 'Link21'))->addClass('treeview-menu')
-                ->add(Link::to('/link22', 'Link22'))
-                ->url('http://www.google.com', 'Google')
-            )
-        )
-//        ->add(
-//            Menu::fullsubmenuexample()
-//        )
-//        ->add(
-//            Menu::adminlteSubmenu('Best menu')
-//                ->add(Link::to('/acacha', 'acacha'))
-//                ->add(Link::to('/profile', 'Profile'))
-//                ->url('http://www.google.com', 'Google')
-//        )
         ->setActiveFromRequest();
 });
