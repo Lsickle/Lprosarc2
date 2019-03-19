71b1157f02662ec7c469a08b4069d7a26877f46c
Andres <andres13marmon@gmail.com>
2019-03-19 09:50:13 -0500

    Arreglo de respel

--- app/Http/Controllers/RespelController.php
+++ app/Http/Controllers/RespelController.php
@@ -63,14 +63,10 @@ class RespelController extends Controller
      */
     public function store(Request $request)
     {   
-        if ($request->hasfile('RespelHojaSeguridad')) {
-            $file = $request->file('RespelHojaSeguridad');
-            $name = time().$file->getClientOriginalName();
-            $file->move(public_path().'/img/', $name);
-        }
-        else{
-            $name = '';
-        }
+        $file = $request->file('RespelHojaSeguridad');
+        $name = time().$file->getClientOriginalName();
+        $file->move(public_path().'/img/', $name);
+        
 
         if ($request->hasfile('RespelTarj')) {
             $file = $request->file('RespelTarj');
@@ -78,17 +74,8 @@ class RespelController extends Controller
             $file->move(public_path().'/img/', $tarj);
         }
         else{
-            $tarj = 'default';
+            $tarj = 'default.png';
         }
-        // if(empty($request->hasfile('RespelHojaSeguridad')) and empty($request->hasfile('RespelHojaSeguridad'))){
-        //     // echo "Inserte La tarjeta de seguridad o la hoja de seguridad";
-        //     "<script>
-        //         alert('Inserte La tarjeta de seguridad o la hoja de seguridad');
-        //     </script>";
-            
-        //     return redirect()->route('respels.create');
-        //     exit;
-        // }
 
         $respel = new Respel();
         $respel->RespelName = $request->input('RespelName');
@@ -102,7 +89,7 @@ class RespelController extends Controller
         $respel->RespelHojaSeguridad = $name;
         $respel->RespelTarj = $tarj;
         $respel->FK_RespelSede = $request->input('FK_RespelSede');
-        $respel->RespelSlug = "slug".$request->input('RespelName').date('Ymd');
+        $respel->RespelSlug = "slug".$request->input('RespelName').date('YmdHis');
         $respel->RespelDelete = 0;
         $respel->save();
 
