9b099c358371170ebf55f76f79cb90658bb2e899
luis de la hoz <davidoy69@hotmail.com>
2019-03-26 12:15:01 -0500

    se ahora se validara el inicio de sesion para todas las demas rutas

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['middleware' => 'auth'], function () {
-    //    Route::get('/link1', function ()    {
-//        // Uses Auth Middleware
-//    });
-
-    //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
-    #adminlte_routes
-
-
-});
 
 Auth::routes(['verify' => true]);
 
@@ -32,47 +22,58 @@ Route::get('/noscriptpage', function () {
     return view('noscriptpage');
 });
 
-Route::resource('/clientes', 'clientcontoller');
-Route::resource('/sclientes', 'sclientcontroller');
-Route::resource('/generadores', 'genercontroller');
-Route::resource('/sgeneradores', 'sgenercontroller');
-Route::resource('/declaraciones', 'DeclarController');
-Route::resource('/respels', 'RespelController');
-Route::resource('/permisos', 'RolesController');
-Route::resource('/audits', 'auditController');
-Route::resource('/place/departament', 'DepartamentoController');
-Route::resource('/areas','AreaController');
-Route::resource('/place/municipal','municipalityController');
-Route::resource('/cargos','CargoController');
-Route::resource('/personal', 'PersonalController');
-Route::resource('/vehicle','VehicleController');
-Route::resource('/vehicle-programacion','VehicProgController');
-Route::resource('/vehicle-mantenimiento','VehicManteController');
-Route::resource('/tratamiento','TratamientoController');
-Route::resource('/asistencia', 'AssistancesController');
-Route::resource('/compra/orden','OrdenCompraController');
-Route::resource('/compra/cotizacion','CotizacionController');
-Route::resource('/activos','ActivoController');
-Route::resource('/activos-movimiento','MovimientoActivoController');
-Route::resource('/capacitacion','TrainingsController');
-Route::resource('/capacitacion-personal','TrainingPersonalsController');
-Route::resource('/inventariotech', 'InventarioTechonologiesController');
-Route::resource('/recibo-material', 'ReciboMaterialController');
-Route::resource('/respel-envios', 'RespelEnviosController');
-Route::resource('/solicitud-residuo', 'SolicitudResiduoController');
-Route::resource('/solicitud-servicio', 'SolicitudServicioController');
-Route::resource('/certificado', 'CertificadoController');
-Route::resource('/manifiesto', 'ManifiestoController');
-Route::resource('/articulos-proveedor', 'ArticuloXProveedorController');
-Route::resource('/code', 'QrCodesController');
-Route::resource('/horario', 'HorarioController');
-Route::resource('/asistencias', 'AsistenciaController');
-Route::resource('/recurso', 'RecursoController');
-Route::resource('/requerimientos', 'RequerimientoController');
-Route::resource('/holidays', 'holidayController');
-Route::get('/prueba', function () {
-    return view('PruebaFullCalendar.index');
-});
-Auth::routes();
+Route::group(['middleware' => 'auth'], function () {
+    //    Route::get('/link1', function ()    {
+//        // Uses Auth Middleware
+//    });
 
-Route::get('/home', 'HomeController@index')->name('home');
+    //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
+    #adminlte_routes
+	Route::resource('/clientes', 'clientcontoller');
+	Route::resource('/sclientes', 'sclientcontroller');
+	Route::resource('/generadores', 'genercontroller');
+	Route::resource('/sgeneradores', 'sgenercontroller');
+	Route::resource('/declaraciones', 'DeclarController');
+	Route::resource('/respels', 'RespelController');
+	Route::resource('/permisos', 'RolesController');
+	Route::resource('/audits', 'auditController');
+	Route::resource('/place/departament', 'DepartamentoController');
+	Route::resource('/areas','AreaController');
+	Route::resource('/place/municipal','municipalityController');
+	Route::resource('/cargos','CargoController');
+	Route::resource('/personal', 'PersonalController');
+	Route::resource('/vehicle','VehicleController');
+	Route::resource('/vehicle-programacion','VehicProgController');
+	Route::resource('/vehicle-mantenimiento','VehicManteController');
+	Route::resource('/tratamiento','TratamientoController');
+	Route::resource('/asistencia', 'AssistancesController');
+	Route::resource('/compra/orden','OrdenCompraController');
+	Route::resource('/compra/cotizacion','CotizacionController');
+	Route::resource('/activos','ActivoController');
+	Route::resource('/activos-movimiento','MovimientoActivoController');
+	Route::resource('/capacitacion','TrainingsController');
+	Route::resource('/capacitacion-personal','TrainingPersonalsController');
+	Route::resource('/inventariotech', 'InventarioTechonologiesController');
+	Route::resource('/recibo-material', 'ReciboMaterialController');
+	Route::resource('/respel-envios', 'RespelEnviosController');
+	Route::resource('/solicitud-residuo', 'SolicitudResiduoController');
+	Route::resource('/solicitud-servicio', 'SolicitudServicioController');
+	Route::resource('/certificado', 'CertificadoController');
+	Route::resource('/manifiesto', 'ManifiestoController');
+	Route::resource('/articulos-proveedor', 'ArticuloXProveedorController');
+	Route::resource('/code', 'QrCodesController');
+	Route::resource('/horario', 'HorarioController');
+	Route::resource('/asistencias', 'AsistenciaController');
+	Route::resource('/recurso', 'RecursoController');
+	Route::resource('/requerimientos', 'RequerimientoController');
+	Route::resource('/holidays', 'holidayController');
+	Route::get('/prueba', function () {
+	    return view('PruebaFullCalendar.index');
+	});
+	Route::resource('/cotizacion', 'CotizacionController');
+
+	Route::get('/home', 'HomeController@index')->name('home');
+
+});
+
+
