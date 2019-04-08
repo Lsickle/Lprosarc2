3337bd5d90aeae478546f99df78e21986f17ac12
luis de la hoz <davidoy69@hotmail.com>
2019-04-08 16:19:16 -0500

    cambiado estilo para paginas de errores

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,27 +1,11 @@
-@extends('adminlte::layouts.errors')
+@extends('errors::illustrated-layout')
 
-@section('htmlheader_title')
-    {{ trans('adminlte_lang::message.servererror') }}
+@section('code', '500')
+@section('title', __('Error'))
+
+@section('image')
+<div style="background-image: url({{ asset('/svg/500.svg') }});" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
+</div>
 @endsection
 
-@section('main-content')
-
-    <div class="error-page">
-        <h2 class="headline text-red">500</h2>
-        <div class="error-content">
-            <h3><i class="fa fa-warning text-red"></i> Oops! {{ trans('adminlte_lang::message.somethingwrong') }}</h3>
-            <p>
-                {{ trans('adminlte_lang::message.wewillwork') }}
-                {{ trans('adminlte_lang::message.mainwhile') }} <a href='{{ url('/home') }}'>{{ trans('adminlte_lang::message.returndashboard') }}</a> {{ trans('adminlte_lang::message.usingsearch') }}
-            </p>
-            <form class='search-form'>
-                <div class='input-group'>
-                    <input type="text" name="search" class='form-control' placeholder="{{ trans('adminlte_lang::message.search') }}"/>
-                    <div class="input-group-btn">
-                        <button type="submit" name="submit" class="btn btn-danger btn-flat"><i class="fa fa-search"></i></button>
-                    </div>
-                </div><!-- /.input-group -->
-            </form>
-        </div>
-    </div><!-- /.error-page -->
-@endsection
+@section('message', __('Whoops, something went wrong on our servers.'))
