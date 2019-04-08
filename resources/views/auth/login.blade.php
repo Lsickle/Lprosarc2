13b7af2b6cbe37ae8c4772e2bddcf2b8c2c352b7
Duvan <programmerloquillo@gmail.com>
2019-04-08 16:14:38 -0500

    Cambiando nombre del proyecto

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,25 +53,22 @@
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
                                     <label class="form-check-label" for="remember">
                                         {{ trans('adminlte_lang::message.remember') }}
                                     </label>
+                                    
                                 </div>
                             </div>
                         </div>
-
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                            <div class="col-md-4 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ trans('adminlte_lang::message.buttonsign') }}
                                 </button>
-
-                                {{-- @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif --}}
+                            </div>
+                            <div class="col-md-4 offset-md-4">
+                                <a href="{{ url('/password/reset') }}">{{ trans('adminlte_lang::message.forgotpassword') }}</a><br>
+                                <a href="{{ url('/register') }}" class="text-center">{{ trans('adminlte_lang::message.registermember') }}</a>
                             </div>
                         </div>
                     </form>
