77ab561137ccacd66a6a9470aac6191ae3effa55
Duvan <programmerloquillo@gmail.com>
2019-04-11 08:45:19 -0500

    Eliminando imagenes, validando create de respels

---
+++ resources/views/layouts/RespelPartials/ClasificacionA.blade.php
@@ -0,0 +1,273 @@
+@extends('layouts.app')
+@section('htmlheader_title', 'Clasificacion A')
+@section('main-content')
+	<div class="container-fluid spark-screen">
+		<div class="row">
+			<div class="col-md-16 col-md-offset-0">
+				<!-- box -->
+				<div class="box">
+					<!-- box-header -->
+					<div class="box-header">
+					<h3 class="box-title">Clasificacion A, segun Decreto Número 4741</h3>
+					</div>
+					<!-- /.box-header -->
+					<!-- box-body -->
+					<div class="box-body">
+					<table id="Clasificacion" class="table table-bordered table-striped">
+						<thead>
+							<tr>
+								<th>Código</th>
+								<th>Descripción</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>A1010</td>
+								<td>Desechos metálicos y desechos que contengan aleaciones de cualquiera de las sustancias siguientes: Antimonio, Arsénico, Berilio, Cadmio, Plomo, Mercurio, Selenio, Telurio, Talio, pero excluidos los desechos que figuran específicamente en la lista B.</td>
+							</tr>
+							<tr>
+								<td>A1020</td>
+								<td>Desechos que tengan como constituyentes o contaminantes, excluidoslos desechos de metal en forma masiva, cualquiera de las sustanciassiguientes:Antimonio; compuestos de antimonio- Berilio; compuestos de berilioCadmio; compuestos de cadmioPlomo; compuestos de plomoSelenio; compuestos de selenioTelurio; compuestos de telurio</td>
+							</tr>
+							<tr>
+								<td>A1030</td>
+								<td>Desechos que tengan comoconstituyentes o contaminantescualquiera de las sustancias siguientes:Arsénico; compuestos dearsénico Mercurio;compuestos de mercurio Talio; compuestos de talio </td>
+							</tr>
+							<tr>
+								<td>A1040</td>
+								<td>Desechos que tengan como constituyentes:- Carbonilos de metalCompuestos de cromohexavalente </td>
+							</tr>
+							<tr>
+								<td>A1050</td>
+								<td>Lodos galvánicos </td>
+							</tr>
+							<tr>
+								<td>A1060</td>
+								<td>Líquidos de desecho del decapaje de metales </td>
+							</tr>
+							<tr>
+								<td>A1070</td>
+								<td>Residuos de lixiviacióndel tratamiento del zinc, polvos y lodos como jarosita,hematites, etc. </td>
+							</tr>
+							<tr>
+								<td>A1080</td>
+								<td>Residuos de desechos de zinc no incluidos en la lista B,que contengan plomo ycadmio en concentraciones tales que presenten características del anexo III del decreto 4741 de 2005 </td>
+							</tr>
+							<tr>
+								<td>A1090</td>
+								<td>Cenizas de la incineración de cables de cobre recubiertos </td>
+							</tr>
+							<tr>
+								<td>A1100</td>
+								<td>Polvos y residuos de los sistemas de depuración de gases delas fundiciones de cobre </td>
+							</tr>
+							<tr>
+								<td>A1110</td>
+								<td>Soluciones electrolíticas usadas de las operaciones derefinación y extracción electrolítica del cobre </td>
+							</tr>
+							<tr>
+								<td>A1120</td>
+								<td>Lodos residuales, excluidos los fangos anódicos, de lossistemas de depuración electrolíticade las operaciones de refinación yextracción electrolítica del cobre </td>
+							</tr>
+							<tr>
+								<td>A1130</td>
+								<td>Soluciones de ácidos paragrabar usadas que contengancobre disuelto </td>
+							</tr>
+							<tr>
+								<td>A1140</td>
+								<td>Desechos de catalizadores de cloruro cúprico y cianuro decobre </td>
+							</tr>
+							<tr>
+								<td>A1150</td>
+								<td>Cenizas de metales preciosos procedentes de la incineración decircuitos impresos no incluidos en la lista B </td>
+							</tr>
+							<tr>
+								<td>A1160</td>
+								<td>Acumuladores de plomo dedesecho, enteros o triturados </td>
+							</tr>
+							<tr>
+								<td>A1170</td>
+								<td>Acumuladores de desecho sin seleccionar excluidas mezclas deacumuladores sólo de lalista B. Los acumuladoresde desecho no incluidosen la lista B que contengan constituyentes del anexo I en tal grado que losconviertan en peligrosos </td>
+							</tr>
+							<tr>
+								<td>A1180</td>
+								<td>Montajes eléctricos y electrónicos de desecho o restos de éstos4quecontengan componentes como acumuladoresy otras baterías incluidos en lalista A, interruptores de mercurio, vidrios de tubos de rayos catódicos y otrosvidrios activados y capacitadores de PCB, o contaminados conconstituyentes del anexo I (por ejemplo,cadmio, mercurio, plomo, bifenilopoliclorado) en tal grado que posean algunade las características del anexoIII (véase la entrada correspondiente en la lista BB1110) rgánica"></td>
+							</tr>
+							<tr>
+								<td>A2010</td>
+								<td>Desechos de vidrio de tubos de rayos catódicos y otros vidrios activados</td>
+							</tr>
+							<tr>
+								<td>A2020</td>
+								<td>Desechos de compuestos inorgánicos de flúor en forma de líquidoso lodos, pero excluidos los desechos de ese tipo especificados en la lista B</td>
+							</tr>
+							<tr>
+								<td>A2030</td>
+								<td>Desechos de catalizadores, pero excluidos los desechos de estetipo especificados en la lista B</td>
+							</tr>
+							<tr>
+								<td>A2040</td>
+								<td>Yeso de desecho procedente de procesosde la industria química, si contiene constituyentes del anexo I en tal grado que presenten una característicapeligrosa del anexo III (véase la entrada correspondiente en la lista B B2080)</td>
+							</tr>
+							<tr>
+								<td>A2050</td>
+								<td>Desechos de amianto (polvo y fibras)</td>
+							</tr>
+							<tr>
+								<td>A2060</td>
+								<td>Cenizas volantes de centraleseléctricas de carbón que contengan sustanciasdelanexo I en concentraciones tales que presenten características del anexo III(véase ia entrada correspondiente en la lista B B2050)</td>
+							</tr>
+							<tr>
+								<td>A3010</td>
+								<td>Desechos resultantes de la producción o el tratamiento de coque de petróleo yasfalto</td>
+							</tr>
+							<tr>
+								<td>A3020</td>
+								<td>Aceites minerales de desecho no aptos para el uso al que estaban destinados</td>
+							</tr>
+							<tr>
+								<td>A3030</td>
+								<td>Desechos que contengan, estén integrados o estén contaminadospor lodos de compuestos antidetonantes con plomo</td>
+							</tr>
+							<tr>
+								<td>A3040</td>
+								<td>Desechos de líquidos térmicos (transferencia de calor)</td>
+							</tr>
+							<tr>
+								<td>A3050</td>
+								<td>Desechos resultantes de laproducción, preparación y utilizaciónde resinas, látex, plastificantes ocolas/adhesivos excepto los desechosespecificados en la lista B (véase elapartado correspondiente en la lista BB4020)</td>
+							</tr>
+							<tr>
+								<td>A3060</td>
+								<td>Nitrocelulosa de desecho</td>
+							</tr>
+							<tr>
+								<td>A3070</td>
+								<td>Desechos de fenoles, compuestos fenólicos, incluido el clorofenol enforma de líquido o de lodo</td>
+							</tr>
+							<tr>
+								<td>A3080</td>
+								<td>Desechos de éteres excepto los especificados en la lista B</td>
+							</tr>
+							<tr>
+								<td>A3090</td>
+								<td>Desechos de cuero en forma de polvo, cenizas, Iodos y harinas que contengancompuestos de plomo hexavalente o biocidas (véase el apartadocorrespondiente en la lista B B3100)</td>
+							</tr>
+							<tr>
+								<td>A3100</td>
+								<td>Raeduras y otros desechos del cuero o de cuero regenerado que nosirvan para la fabricación de artículos de cuero, que contengan compuestos decromo hexavalente o biocidas (véase elapartado correspondiente en la lista BB3090)</td>
+							</tr>
+							<tr>
+								<td>A3110</td>
+								<td>Desechos del curtido de pieles que contengan compuestos de cromohexavalente o biocidas o sustanciasinfecciosas (véase el apartadocorrespondiente en la lista B B3110)</td>
+							</tr>
+							<tr>
+								<td>A3120</td>
+								<td>Pelusas - fragmentos ligeros resultantes deldesmenuzamiento</td>
+							</tr>
+							<tr>
+								<td>A3130</td>
+								<td>Desechos de compuestos de fósforoorgánicos</td>
+							</tr>
+							<tr>
+								<td>A3140</td>
+								<td>Desechos de disolventesorgánicos no halogenados pero conexclusión de los desechos especificados en la lista B</td>
+							</tr>
+							<tr>
+								<td>A3150</td>
+								<td>Desechos de disolventes orgánicos halogenados</td>
+							</tr>
+							<tr>
+								<td>A3160</td>
+								<td>Desechos resultantes de residuosno acuosos de destilación halogenados o nohalogenados derivados de operaciones de recuperación de disolventesorgánicos</td>
+							</tr>
+							<tr>
+								<td>A3170</td>
+								<td>Desechos resultantes de laproducción de hidrocarburos halogenadosalifáticos (tales como clorometano, dicloroetano, cloruro de vinilo, cloruro dealilo y epicloridrina)</td>
+							</tr>
+							<tr>
+								<td>A3180</td>
+								<td>Desechos, sustancias y artículos que contienen, consisten o estáncontaminados con bifenilo policlorado (PCB), terfenilo policlorado (PCT),naftaleno policlorado (PCN) o bifenilopolibromado (PBB), o cualquier otrocompuesto polibromado análogo, con unaconcentración de igual o superior a50 mg/kg</td>
+							</tr>
+							<tr>
+								<td>A3190</td>
+								<td>Desechos de residuos alquitranados (con exclusión de loscementos asfálticos) resultantes de larefinación, destilación o cualquierotro tratamiento pirolíticode materiales orgánicos</td>
+							</tr>
+							<tr>
+								<td>A3200</td>
+								<td>Material bituminoso (desechosde asfalto) con contenido de alquitránresultantes de la construcción y e! mantenimiento de carreteras (obsérvese elartículo correspondiente B2130de la lista B)</td>
+							</tr>
+							<tr>
+								<td>A4010</td>
+								<td>Desechos resultantes de laproducción, preparación y utilizaciónde productos farmacéuticos, pero con exclusión de los desechosespecificados en la lista B</td>
+							</tr>
+							<tr>
+								<td>A4020</td>
+								<td>Desechos clínicos y afines; esdecir desechos resultantes de prácticasmédicas, de enfermería, dentales, veterinarias o actividades similares, ydesechos generados en hospitales u otrasinstalaciones durante actividades deinvestigación o el tratamiento de pacientes, o de proyectos de investigación</td>
+							</tr>
+							<tr>
+								<td>A4030</td>
+								<td>Desechos resultantes de la producción, ¡a preparación y la utilizaciónde biocidas y productos fitofarmacéuticos, con inclusión de desechos deplaguicidas y herbicidas que no respondan a las especificaciones, caducados7,en desuso8o no aptos para el uso previsto originalmente.</td>
+							</tr>
+							<tr>
+								<td>A4040</td>
+								<td>Desechos resultantes de lafabricación, preparación y utilizaciónde productos químicos para lapreservación de la madera</td>
+							</tr>
+							<tr>
+								<td>A4050</td>
+								<td>Desechos que contienen, consisten o están contaminados conalgunos de los productos siguientes:Cianuros inorgánicos, con excepción de residuos que contienen metalespreciosos, en forma sólida, con trazas de cianuros inorgánicosCianuros orgánicos</td>
+							</tr>
+							<tr>
+								<td>A4060</td>
+								<td>Desechos demezclas y emulsiones de aceite y aguao de hidrocarburos y agua</td>
+							</tr>
+							<tr>
+								<td>A4070</td>
+								<td>Desechos resultantes de la producción, preparación y utilización detintas, colorantes, pigmentos, pinturas,lacas o barnices, con exclusión de losdesechos especificados en la lista B (véase el apartado correspondiente de lalista B B4010)</td>
+							</tr>
+							<tr>
+								<td>A4080</td>
+								<td>Desechos de carácter explosivo (pero con exclusión de los desechosespecificados en la lista B)</td>
+							</tr>
+							<tr>
+								<td>A4090</td>
+								<td>Desechos de soluciones ácidas o básicas, distintas de las especificadas en elapartado correspondiente de la lista B (véase el apartado correspondiente dela lista B B2120)</td>
+							</tr>
+							<tr>
+								<td>A4100</td>
+								<td>Desechos resultantes de la utilización de dispositivos de control de íacontaminación industrial para la depuración de los gases industriales, pero conexclusión de los desechos especificados en la lista B</td>
+							</tr>
+							<tr>
+								<td>A4110</td>
+								<td>Desechos que contienen, consisten o, están contaminados conalgunos de los productos siguientes:- - Cualquier sustancia de! grupo de los dibenzofuranospoliclorados- - Cualquier sustancia del grupo de las dibenzodioxinas policloradas</td>
+							</tr>
+							<tr>
+								<td>A4120</td>
+								<td>Desechos que contienen, consisten o están contaminados con peróxidos</td>
+							</tr>
+							<tr>
+								<td>A4130</td>
+								<td>Envases y contenedores dedesechos que contienen sustanciasincluidas en el anexo I, en concentraciones suficientes como para mostrar lascaracterísticas peligrosas del anexo III</td>
+							</tr>
+							<tr>
+								<td>A4140</td>
+								<td>Desechos consistentes o que contienenproductos químicos que noresponden a las especificaciones o caducados10correspondientes a lascategorías del anexo I, y que muestran las características peligrosas del anexo III</td>
+							</tr>
+							<tr>
+								<td>A4150</td>
+								<td>Sustancias químicas de desecho, no identificadas o nuevas, resultantes de iainvestigación y el desarrollo o de las actividades de enseñanza y cuyos efectosen el ser humano o el medio ambiente no se conozcan</td>
+							</tr>
+							<tr>
+								<td>A4160</td>
+								<td>Carbono activado consumido noincluido en ¡a ¡ista B (véase elcorrespondiente apartado de la lista B B2060).</td>
+							</tr>
+						</tbody>
+					</table>
+					</div>
+					<!-- /.box-body -->
+				</div>
+				<!-- /.box -->
+			</div>
+		</div>
+	</div>
+@endsection
