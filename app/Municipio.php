c2fa2ad6499d29f322433763efb6634784a1e2a6
luis de la hoz <davidoy69@hotmail.com>
2019-04-16 10:43:54 -0500

    compilando a produccion para prueba en servidor

--- app/Municipio.php
+++ app/Municipio.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Municipio extends Model
 {
-    protected $table = 'Municipios';
+    protected $table = 'municipios';
 
     protected $fillable = ['MunName'];
     
