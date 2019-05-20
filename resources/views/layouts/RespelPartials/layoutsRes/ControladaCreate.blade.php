31c680dad5100c656e532e95eabf2b4dbd070faa
luis de la hoz <davidoy69@hotmail.com>
2019-05-20 10:53:52 -0500

    ampliando el create de residuos

---
+++ resources/views/layouts/RespelPartials/layoutsRes/ControladaCreate.blade.php
@@ -0,0 +1,35 @@
+<input type="text" hidden="" name="SControlada[]" value="">
+<label data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" title="<b>Nombre de la sustancia A</b>" data-content="<p style='width: 50%'> Si el campo <b><i>Peligrosidad del residuo</i></b> es diferente a: <i>No peligroso</i>, entonces, este campo es Obligatorio</p>">Nombre de la sustancia, segun
+	<a href="{{route('ClasificacionA')}}" target="_blank"> Resolución Número 1 del 2015 (Consejo nacional de estupefacientes)<i style="font-size: 1.8rem; color: Dodgerblue;" class="fas fa-info-circle fa-2x fa-spin"></i></a>
+</label>
+<select name="SustanciaControladaNombre[]" class="form-control" required>
+	<option value="Acetato de butilo">Acetato de butilo</option>
+	<option value="Acetato de etilo">Acetato de etilo</option>
+	<option value="Acetato de isobutilo">Acetato de isobutilo</option>
+	<option value="Acetato de isopropilo">Acetato de isopropilo</option>
+	<option value="Acetato de n-propilo">Acetato de n-propilo</option>
+	<option value="Acetona">Acetona</option>
+	<option value="Ácido clorhídrico">Ácido clorhídrico</option>
+	<option value="Ácido sulfúrico">Ácido sulfúrico</option>
+	<option value="Alcohol isopropílico">Alcohol isopropílico</option>
+	<option value="Amoniaco">Amoniaco</option>
+	<option value="Anhídrido acético">Anhídrido acético</option>
+	<option value="Butanol">Butanol</option>
+	<option value="Carbonato de sodio">Carbonato de sodio</option>
+	<option value="Cloroformo">Cloroformo</option>
+	<option value="Cloruro de calcio">Cloruro de calcio</option>
+	<option value="Diacetona alcohol">Diacetona alcohol</option>
+	<option value="Dióxido de manganeso">Dióxido de manganeso</option>
+	<option value="Disolvente números 1 y 1A">Disolvente números 1 y 1A</option>
+	<option value="Disolvente número 2">Disolvente número 2</option>
+	<option value="Éter etílico">Éter etílico</option>
+	<option value="Hexano">Hexano</option>
+	<option value="Manganato de potasio">Manganato de potasio</option>
+	<option value="Metanol">Metanol</option>
+	<option value="Metabisulfito de sodio">Metabisulfito de sodio</option>
+	<option value="Metil etil cetona">Metil etil cetona</option>
+	<option value="Metil isobutil cetona">Metil isobutil cetona</option>
+	<option value="Permanganato de potasio">Permanganato de potasio</option>
+	<option value="Thinner">Thinner</option>
+	<option value="Tolueno">Tolueno</option>
+</select>
--- resources/views/layouts/RespelPartials/respelform1.blade.php
+++ resources/views/layouts/RespelPartials/respelform1.blade.php
@@ -46,7 +46,17 @@
 			    <label data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" title="<b>Hoja de seguridad</b>" data-content="<p style='width: 50%'> Si el campo <b><i>Peligrosidad del residuo</i></b> es diferente a: <i>No peligroso</i>, entonces, este campo es Obligatorio... sin embargo, podra postponer la carga de la <b>Tarjeta de Emergencia</b> hasta el momento en el que vaya a realizar un solicitud de servicio</p>">Tarjeta De Emergencia <i  style="font-size: 1.8rem; color: Dodgerblue;" class="fas fa-info-circle fa-2x fa-spin"></i></label>
 			    <input name="RespelTarj[]" type="file" class="form-control" accept=".pdf">
 			</div> 
-			
+			<div class="col-md-6 form-group">
+				<label>¿Sustancia controlada?</label>
+				<select id="selectDanger0" name="SustanciaControlada[]" class="form-control" required>
+					<option onclick="setNoControlada(0)">No</option>
+					<option onclick="setControlada(0)">Si</option> 
+				</select>
+			</div>
+
+			<div id="SustanciaControlada0">
+				
+			</div>
 		{{-- </div> --}}
 	</div>
 </div>
@@ -71,6 +81,20 @@
 	    $("#danger"+id).empty();
 	    $("#hoja"+id).prop('required', false)
 	}
+
+	function setControlada(id){
+		var ifDangerRespel = `@include('layouts.RespelPartials.layoutsRes.ifControladaRespel')`;
+	    $("#SustanciaControlada"+id).empty();
+	    $("#SustanciaControlada"+id).append(ifDangerRespel);
+	    $("#hoja"+id).prop('required', true);
+	    attachPopover();
+	}
+	function setNoControlada(id){
+	    $("#SustanciaControlada"+id).empty();
+	    $("#hoja"+id).prop('required', false)
+	}
+	var Controlada = `@include('layouts.RespelPartials.layoutsRes.ControladaCreate')`;
+	var Masivo = `@include('layouts.RespelPartials.layoutsRes.MasivoCreate')`;
 	var ClasifY = `@include('layouts.RespelPartials.layoutsRes.ClasificacionYCreate')`;
 	var ClasifA = `@include('layouts.RespelPartials.layoutsRes.ClasificacionACreate')`;
 	function AgregarRes(){
@@ -99,6 +123,26 @@
 		$("#Clasif"+id).empty();
 		$("#Clasif"+id).append(ClasifA);
 		$("#myform").validator('update');
+		attachPopover();
+	}
+	function AgregarControlada(id){
+		$("#ClasifY"+id).removeClass("btn-default");
+		$("#ClasifY"+id).addClass("btn-success");
+		$("#ClasifA"+id).removeClass("btn-success");
+		$("#ClasifA"+id).addClass("btn-default");
+		$("#Clasif"+id).empty();
+		$("#Clasif"+id).append(Controlada);
+		$("#myform").validator('update');
+		attachPopover();
+	}
+	function AgregarMasivo(id){
+		$("#ClasifA"+id).removeClass("btn-default");
+		$("#ClasifA"+id).addClass("btn-success");
+		$("#ClasifY"+id).removeClass("btn-success");
+		$("#ClasifY"+id).addClass("btn-default");
+		$("#Clasif"+id).empty();
+		$("#Clasif"+id).append(ClasifA);
+		$("#myform").validator('Masivo');
 		attachPopover();
 	}
 	function EliminarRes(id){
