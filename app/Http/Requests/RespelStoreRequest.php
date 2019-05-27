7e904621840acc6d1e2634dfe7bb1e8d51d43487
luis de la hoz <davidoy69@hotmail.com>
2019-05-27 09:22:51 -0500

    se verifica si existe el campo foto en el request

--- app/Http/Requests/RespelStoreRequest.php
+++ app/Http/Requests/RespelStoreRequest.php
@@ -30,32 +30,47 @@ class RespelStoreRequest extends FormRequest
     public function rules()
         {
             $request = $this->instance()->all();
-            $images = $request['RespelFoto'];
+            if (isset($request['RespelFoto'])) {
+                $images = $request['RespelFoto'];
+                foreach($images as $key => $file) {
+                    $rules['RespelFoto.'.$key] = 'max:1024|mimes:jpeg,png';
+                }
+            }
+
+            // $nombresderesiduos = $request['RespelFoto'];
 
             $rules = [
-                // 'RespelFoto' => 'image'
+                'RespelName.*' => 'max:128|alpha_num',
+                'RespelDescrip.*' => 'max:512|alpha_num',
+                'RespelIgrosidad.*' => 'max:12',
+                'RespelEstado.*' => 'max:12|alpha',
+                'RespelHojaSeguridad.*' => 'max:1024|mimes:pdf',
+                'YRespelClasf4741.*' => 'sometimes|max:12',
+                'ARespelClasf4741.*' => 'sometimes|max:12',
+                'RespelTarj.*' => 'sometimes|max:1024|mimes:pdf',
+                'SustanciaControlada.*' => 'max:12|alpha_num',
+                'SustanciaControladaNombre.*' => 'max:12|alpha_num',
+                'SustanciaControladaDocumento.*' => 'max:1024|mimes:pdf',
+
             ];
-            foreach($images as $key => $file) {
-                $rules['RespelFoto.'.$key] = 'max:1024|mimes:jpeg,png';
-            }
+
+            
             return $rules;
         }
 
     public function attributes()
     {   
         $request = $this->instance()->all();
-        $images = $request['RespelFoto'];
-        // return [
-        //     'RespelFoto' => 'Fotografía',
-        // ];
-
         $attributes = [];
-        // for ($i=0; $i < count($this->request->get('RespelFoto')) ; $i++) { 
-            foreach($images as $key => $value)
-            {
+
+        
+        if (isset(var) $request['RespelFoto']) {
+            $images = $request['RespelFoto'];
+            foreach($images as $key => $value){
                 $attributes['RespelFoto.'.$key] = '"Fotografía N° '.($key+1).'"';
             }
-        // }
+        }
+
 
         return $attributes;
     }
@@ -63,15 +78,20 @@ class RespelStoreRequest extends FormRequest
     public function messages()
     {
         $request = $this->instance()->all();
-        $images = $request['RespelFoto'];
+        if (isset($request['RespelFoto'])) {
+            $imagesmesage = $request['RespelFoto'];
+            foreach($imagesmesage as $key => $file) {
+                $messages['RespelFoto.'.$key.'.mimes'] = 'el archivo que adjuntó en el campo "Fotografía" del Residuo N° .'.($key+1).' no corresponde con los formatos permitidos: jpg, :values ';
+                $messages['RespelFoto.'.$key.'.max'] = 'el archivo que adjuntó en el campo "Fotografía" del Residuo N° .'.($key+1).' excede el máximo permitido de :max Kilobytes';
+            }
+        }
         $messages = [
             // 'RespelFoto.required' => 'You must upload a file.'
         ];
 
-        foreach($images as $key => $file) {
-            $messages['RespelFoto.'.$key.'.mimes'] = 'el archivo que adjuntó en el campo "Fotografía" del Residuo N° .'.($key+1).' no corresponde con los formatos permitidos: jpg, :values ';
-            $messages['RespelFoto.'.$key.'.max'] = 'el archivo que adjuntó en el campo "Fotografía" del Residuo N° .'.($key+1).' excede el máximo permitido de :max Kilobytes';
-        }
+        
+
+        
         return $messages;
     }
     // protected function failedValidation(Validator $validator)
