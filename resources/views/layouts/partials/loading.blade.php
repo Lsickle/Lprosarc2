4cfbbc5a23c556842e1af1b1cacd8e74d8add748
Duvan <programmerlloquillo@gmail.com>
2019-06-10 21:58:59 -0500

    Arreglando loading del proyecto

--- resources/views/layouts/partials/loading.blade.php
+++ resources/views/layouts/partials/loading.blade.php
@@ -1,7 +1,17 @@
 <div id="contenedor_carga">
-	<div id="Superior"></div>
-	<div id="Derecho"></div>
-	<div id="Izquierdo"></div>
-	<div id="Inferior1"></div>
-	<div id="Inferior2"></div>
+	<div id="Superior">
+		<img src="/img/LoadingProsarc/Superior.png">
+	</div>
+	<div id="Derecho">
+		<img src="/img/LoadingProsarc/Derecho.png">
+	</div>
+	<div id="Izquierdo">
+		<img src="/img/LoadingProsarc/Izquierdo.png">
+	</div>
+	<div id="Inferior1">
+		<img src="/img/LoadingProsarc/Inferior1.png">
+	</div>
+	<div id="Inferior2">
+		<img src="/img/LoadingProsarc/Inferior2.png">
+	</div>
 </div>
