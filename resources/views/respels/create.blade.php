de422850baeccf1674c842f5fd5ec88d2c511244
luis de la hoz <davidoy69@hotmail.com>
2019-06-12 12:37:49 -0500

    se intercambian los colores de los botones de crear y añadir residuo

--- resources/views/respels/create.blade.php
+++ resources/views/respels/create.blade.php
@@ -49,8 +49,8 @@
 								<!-- /.box-body -->
 								<div class="col-md-12">	
 									<div class="box-footer">
-										<button type="submit" class="btn btn-primary"><i class="fa fa-check"></i> Registrar</button>
-										<a onclick="AgregarRes()" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Agregar Residuo</a>	
+										<button type="submit" class="btn btn-success"><i class="fa fa-check"></i> Registrar</button>
+										<a onclick="AgregarRes()" class="btn btn-primary pull-right"><i class="fa fa-plus"></i> Agregar Residuo</a>	
 									</div>
 								</div>
 							</form>
