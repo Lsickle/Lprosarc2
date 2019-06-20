83027530a873f12acf00df399d709e090b5f7655
luis de la hoz <davidoy69@hotmail.com>
2019-06-20 11:15:09 -0500

    se comenta el checkbox remember de la vista login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,7 +51,7 @@
                             </div>
                         </div>
                         
-                        <div class="form-group row">
+                      {{--   <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -61,7 +61,7 @@
                                     </label>
                                 </div>
                             </div>
-                        </div>
+                        </div> --}}
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
