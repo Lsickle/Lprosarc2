2d64ebd2890dca9e9d46359284acdd8a05370201
Duvan <programmerloquillo@gmail.com>
2019-07-03 11:15:27 -0500

    Arreglando vista del edit del vehiculo

--- resources/views/vehicle/edit.blade.php
+++ resources/views/vehicle/edit.blade.php
@@ -9,7 +9,6 @@
 <div class="container-fluid spark-screen">
 	<div class="row">
 		<div class="col-md-16 col-md-offset-0">
-			<!-- Default box -->
 			<div class="box">
 				<div class="box-header">
 					@component('layouts.partials.modal')
@@ -36,76 +35,50 @@
 					</form>
 					@endif
 				</div>
-				<div class="row">
-					<!-- left column -->
-					<div class="col-md-12">
-						<!-- general form elements -->
-						<div class="box box-primary">
-							<!-- /.box-header -->
-							<!-- form start -->
-							<form role="form" action="/vehicle/{{$Vehicle->VehicPlaca}}" method="POST" enctype="multipart/form-data">
-								@method('PUT')
-								@csrf
-								<div class="box-body">
-									<div class="col-md-6">
-										<label for="VehicPlaca">Numero de placa</label>
-										<input type="text" class="form-control" id="VehicPlaca" name="VehicPlaca" required="true" value="{{$Vehicle->VehicPlaca}}">
-									</div>
-									<div class="col-md-6">
-										<label for="VehicTipo">Tipo de vehiculo</label>
-										<input type="text" class="form-control" id="VehicTipo" name="VehicTipo" maxlength="16" value="{{$Vehicle->VehicTipo}}">
-									</div>
-									<div class="col-md-6">
-										<label for="VehicCapacidad">Capacidad (Kilos)</label>
-										<input type="number" class="form-control" id="VehicCapacidad" name="VehicCapacidad" max="999999" value="{{$Vehicle->VehicCapacidad}}">
-									</div>
-									<div class="col-md-6">
-										<label for="VehicKmActual">Kilometraje actual</label>
-										<input type="number" class="form-control" id="VehicKmActual" name="VehicKmActual" required="true" max="999999" value="{{$Vehicle->VehicKmActual}}">
-									</div>
-									<div class="col-md-6">
-											<label for="FK_VehiSede">Sede</label>
-											<select class="form-control" id="FK_VehiSede" name="FK_VehiSede" required="true">
-												<option value="{{$Vehicle->FK_VehiSede}}">Seleccione...</option>
-												@foreach($Sedes as $Sede)
-													<option value="{{$Sede->ID_Sede}}">{{$Sede->SedeName}}</option>
-												@endforeach
-											</select>
-									</div>
-									<div class="col-md-6">
-										<label for="VehicInternExtern">Vehiculo</label>
-										@if($Vehicle->VehicInternExtern === 1)
-											<select class="form-control" id="VehicInternExtern" name="VehicInternExtern" required="true">
-												<option value="1">Interno</option>
-												<option value="0">Externo</option>
-											</select>
-										@else
-											<select class="form-control" id="VehicInternExtern" name="VehicInternExtern" required="true">
-												<option value="0">Externo</option>
-												<option value="1">Interno</option>
-											</select>
-										@endif
-									</div>
-								</div>
-								<!-- /.box-body -->
-								<div class="box-footer">
-									<button type="submit" class="btn btn-primary">Actualizar</button>
-								</div>
-							</form>
+				<div class="box box-info">
+					<form role="form" action="/vehicle/{{$Vehicle->VehicPlaca}}" method="POST" enctype="multipart/form-data" data-toggle="validator">
+						@method('PUT')
+						@csrf
+						<div class="box-body">
+							<div class="form-group col-md-12">
+									<label for="FK_VehiSede">Sede</label>
+									<small class="help-block with-errors">*</small>
+									<select class="form-control" id="FK_VehiSede" name="FK_VehiSede" required="true">
+										<option value="{{$Vehicle->FK_VehiSede}}">Seleccione...</option>
+										@foreach($Sedes as $Sede)
+											<option value="{{$Sede->ID_Sede}}" {{$Vehicle->FK_VehiSede == $Sede->ID_Sede ? 'selected' : ''}}>{{$Sede->SedeName}}</option>
+										@endforeach
+									</select>
+							</div>
+							<div class="form-group col-md-6">
+								<label for="VehicPlaca">Numero de placa</label>
+								<small class="help-block with-errors">*</small>
+								<input type="text" class="form-control placa" id="VehicPlaca" name="VehicPlaca" required="true" data-minlength="7" value="{{$Vehicle->VehicPlaca}}">
+							</div>
+							<div class="form-group col-md-6">
+								<label for="VehicTipo">Tipo de vehiculo</label>
+								<small class="help-block with-errors">*</small>
+								<input type="text" class="form-control" id="VehicTipo" name="VehicTipo" maxlength="16" value="{{$Vehicle->VehicTipo}}">
+							</div>
+							<div class="form-group col-md-6">
+								<label for="VehicCapacidad">Capacidad (Kilos)</label>
+								<small class="help-block with-errors">*</small>
+								<input type="number" class="form-control" id="VehicCapacidad" name="VehicCapacidad" max="999999" value="{{$Vehicle->VehicCapacidad}}">
+							</div>
+							<div class="form-group col-md-6">
+								<label for="VehicKmActual">Kilometraje actual</label>
+								<small class="help-block with-errors">*</small>
+								<input type="number" class="form-control" id="VehicKmActual" name="VehicKmActual" required="true" max="999999" value="{{$Vehicle->VehicKmActual}}">
+							</div>
 						</div>
-						<!-- /.box -->
-					</div>
-					</div>
-					</div>
-					</div>
-					<!-- /.box-body -->
+						<!-- /.box-body -->
+						<div class="box-footer">
+							<button type="submit" class="btn btn-warning pull-right">{{trans('adminlte_lang::message.update')}}</button>
+						</div>
+					</form>
 				</div>
-				<!-- /.box -->
 			</div>
-			<!--/.col (right) -->
 		</div>
-		<!-- /.box-body -->
 	</div>
-	<!-- /.box -->
 </div>
 @endsection
