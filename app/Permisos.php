4a4d7c47cd5a638e50040cadda6bbc11ae7c8a5c
Duvan <programmerloquillo@gmail.com>
2019-07-04 11:33:58 -0500

    Creando clase de permisos

---
+++ app/Permisos.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+class Permisos{
+    const Array = ['Programador'];
+}
+/*
+Programador
+AdministradorPlanta
+Hseq
+JefeLogistica
+AsistenteLogistica
+Conductor
+JefeOperaciones
+Supervisor
+AdministradorBogota
+JefeComercial
+Tesorería
+Comercial
+AsistenteComercial
+Cliente
+ */
