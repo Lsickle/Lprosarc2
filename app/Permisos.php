bcf7fd82818e8728cd3b2e27d0d7d42c87dd81b3
luis de la hoz <davidoy69@hotmail.com>
2019-07-05 08:18:09 -0500

    se cambia las validaciones de rol de las vistas y controladores de cliente

--- app/Permisos.php
+++ app/Permisos.php
@@ -4,6 +4,28 @@ namespace App;
 
 class Permisos{
     const Menu1 = ['Programador', 'AdministradorPlanta', 'JefeLogistica', 'JefeOperaciones', 'AdministradorBogota', 'JefeComercial'];
+    /*using: ClienteController:index,show,edit cliencontoller:index,show,edit clientes/create2 clientes/index clientes/show clientes/edit*/
+    const CLIENTE = ['Cliente'];
+    /*ClienteController:index,show,edit*/
+    const PROGRAMADOR = ['Programador'];
+    /*cliencontoller:index,show,edit*/
+    const TODOPROSARC = [
+    	'Programador',
+		'AdministradorPlanta',
+		'Hseq',
+		'JefeLogistica',
+		'AsistenteLogistica',
+		'Conductor',
+		'JefeOperaciones',
+		'Supervisor',
+		'AdministradorBogota',
+		'JefeComercial',
+		'Tesorería',
+		'Comercial',
+		'AsistenteComercial',
+		'Cliente'
+	];
+
 }
 /*
 Programador
