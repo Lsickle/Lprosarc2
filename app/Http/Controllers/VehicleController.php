6b8e6a429073b305e59f37e2b1c16a126551aca5
LoquilloDuvan <programmerloquillo@gmail.com>
2019-07-05 16:40:35 -0500

    Arreglando roles del edit de los vehiculos internos

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -11,147 +11,154 @@ use Permisos;
 
 class VehicleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    { 
-        if(Auth::user()->UsRol <> trans('adminlte_lang::message.Cliente') ||Auth::user()->UsRol <> trans('adminlte_lang::message.Cliente')){
-            $Vehicles = DB::table('vehiculos')
-                ->Join('sedes', 'vehiculos.FK_VehiSede', '=', 'sedes.ID_Sede')
-                ->select('vehiculos.*', 'sedes.SedeName')
-                ->where(function($query){
-                    if(Auth::user()->UsRol <> trans('adminlte_lang::message.Programador')){
-                        $query->where('VehicDelete', 0);
-                    }
-                })
-                ->get();
-            return view('vehicle.index', compact('Vehicles'));
-        }
-        else{
-            abort(403);
-        }
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{ 
+		if(Auth::user()->UsRol <> trans('adminlte_lang::message.Cliente') ||Auth::user()->UsRol <> trans('adminlte_lang::message.Cliente')){
+			$Vehicles = DB::table('vehiculos')
+				->Join('sedes', 'vehiculos.FK_VehiSede', '=', 'sedes.ID_Sede')
+				->select('vehiculos.*', 'sedes.SedeName')
+				->where(function($query){
+					if(Auth::user()->UsRol <> trans('adminlte_lang::message.Programador')){
+						$query->where('VehicDelete', 0);
+					}
+				})
+				->get();
+			return view('vehicle.index', compact('Vehicles'));
+		}
+		else{
+			abort(403);
+		}
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        if(in_array(Auth::user()->UsRol, Permisos::ProgVehic1) || in_array(Auth::user()->UsRol2, Permisos::ProgVehic1)){
-            $Sedes = DB::table('sedes')
-                ->select('ID_Sede', 'SedeName')
-                ->where('SedeDelete', 0)
-                ->get();
-            return view('vehicle.create', compact('Sedes'));
-        }
-        else{
-            abort(403);
-        }
-    }
-    
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $Vehicle = new Vehiculo();
-        $Vehicle->VehicPlaca = $request->input('VehicPlaca');
-        $Vehicle->VehicCapacidad = $request->input('VehicCapacidad');
-        $Vehicle->VehicKmActual = $request->input('VehicKmActual');
-        $Vehicle->VehicTipo = $request->input('VehicTipo');
-        $Vehicle->FK_VehiSede = $request->input('FK_VehiSede');
-        $Vehicle->VehicInternExtern = 1;
-        $Vehicle->VehicDelete = 0;
-        $Vehicle->save();
-        
-        return redirect()->route('vehicle.index');
-    }
-    
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {   
-        //
-    }
-    
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $Vehicle = Vehiculo::where('VehicPlaca', $id)->first();
-        $Sedes = DB::table('sedes')
-            ->select('ID_Sede', 'SedeName')
-            ->where('SedeDelete', 0)
-            ->get();
-        return view('vehicle.edit', compact('Vehicle','Sedes'));
-    }
-    
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $Vehicle = Vehiculo::where('VehicPlaca', $id)->first();
-        $Vehicle->fill($request->all());
-        $Vehicle->VehicInternExtern = 1;
-        $Vehicle->save();
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		if(in_array(Auth::user()->UsRol, Permisos::ProgVehic1) || in_array(Auth::user()->UsRol2, Permisos::ProgVehic1)){
+			$Sedes = DB::table('sedes')
+				->select('ID_Sede', 'SedeName')
+				->where('FK_SedeCli', userController::IDClienteSegunUsuario())
+				->where('SedeDelete', 0)
+				->get();
+			return view('vehicle.create', compact('Sedes'));
+		}
+		else{
+			abort(403);
+		}
+	}
+	
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		$Vehicle = new Vehiculo();
+		$Vehicle->VehicPlaca = $request->input('VehicPlaca');
+		$Vehicle->VehicCapacidad = $request->input('VehicCapacidad');
+		$Vehicle->VehicKmActual = $request->input('VehicKmActual');
+		$Vehicle->VehicTipo = $request->input('VehicTipo');
+		$Vehicle->FK_VehiSede = $request->input('FK_VehiSede');
+		$Vehicle->VehicInternExtern = 1;
+		$Vehicle->VehicDelete = 0;
+		$Vehicle->save();
+		
+		return redirect()->route('vehicle.index');
+	}
+	
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{   
+		//
+	}
+	
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{
+		if(in_array(Auth::user()->UsRol, Permisos::ProgVehic1) || in_array(Auth::user()->UsRol2, Permisos::ProgVehic1)){
+			$Vehicle = Vehiculo::where('VehicPlaca', $id)->first();
+			$Sedes = DB::table('sedes')
+				->select('ID_Sede', 'SedeName')
+				->where('FK_SedeCli', userController::IDClienteSegunUsuario())
+				->where('SedeDelete', 0)
+				->get();
+			return view('vehicle.edit', compact('Vehicle','Sedes'));
+		}
+		else{
+			abort(403);
+		}
+	}
+	
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		$Vehicle = Vehiculo::where('VehicPlaca', $id)->first();
+		$Vehicle->fill($request->all());
+		$Vehicle->VehicInternExtern = 1;
+		$Vehicle->save();
 
-        $log = new audit();
-        $log->AuditTabla="vehiculos";
-        $log->AuditType="Modificado";
-        $log->AuditRegistro=$Vehicle->VehicPlaca;
-        $log->AuditUser=Auth::user()->email;
-        $log->Auditlog=$request->all();
-        $log->save();
+		$log = new audit();
+		$log->AuditTabla="vehiculos";
+		$log->AuditType="Modificado";
+		$log->AuditRegistro=$Vehicle->VehicPlaca;
+		$log->AuditUser=Auth::user()->email;
+		$log->Auditlog=$request->all();
+		$log->save();
 
-        return redirect()->route('vehicle.index');
-    }
+		return redirect()->route('vehicle.index');
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $Vehicle = Vehiculo::where('VehicPlaca', $id)->first();
-            if ($Vehicle->VehicDelete == 0) {
-                $Vehicle->VehicDelete = 1;
-            }
-            else{
-                $Vehicle->VehicDelete = 0;
-            }
-        $Vehicle->save();
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		$Vehicle = Vehiculo::where('VehicPlaca', $id)->first();
+			if ($Vehicle->VehicDelete == 0) {
+				$Vehicle->VehicDelete = 1;
+			}
+			else{
+				$Vehicle->VehicDelete = 0;
+			}
+		$Vehicle->save();
 
-        $log = new audit();
-        $log->AuditTabla = "vehiculos";
-        $log->AuditType = "Eliminado";
-        $log->AuditRegistro = $Vehicle->VehicPlaca;
-        $log->AuditUser = Auth::user()->email;
-        $log->Auditlog = $Vehicle->VehicDelete;
-        $log->save();
+		$log = new audit();
+		$log->AuditTabla = "vehiculos";
+		$log->AuditType = "Eliminado";
+		$log->AuditRegistro = $Vehicle->VehicPlaca;
+		$log->AuditUser = Auth::user()->email;
+		$log->Auditlog = $Vehicle->VehicDelete;
+		$log->save();
 
-        return redirect()->route('vehicle.index');
-    }
+		return redirect()->route('vehicle.index');
+	}
 }
