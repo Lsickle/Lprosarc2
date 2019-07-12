17cd9a9d05e2ec76f450c5aac12dc8aaac18b76b
Andres <andres13marmon@gmail.com>
2019-07-12 09:58:34 -0500

    Modificando emailcontroller para los dos roles

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -22,38 +22,23 @@ class EmailController extends Controller
                 ->select('personals.PersEmail', 'personals.PersFirstName', 'personals.PersLastName', 'clientes.CliName', 'solicitud_servicios.*')
                 ->where('solicitud_servicios.SolSerSlug', '=', $SolSer->SolSerSlug)
                 ->first();
-            
-            $emailUser = DB::table('solicitud_servicios')
-                ->join('clientes', 'clientes.ID_Cli', '=', 'solicitud_servicios.FK_SolSerCliente')
-                ->join('sedes', 'sedes.FK_SedeCli', '=' ,'clientes.ID_Cli')
-                ->join('areas', 'areas.FK_AreaSede', '=', 'sedes.ID_Sede')
-                ->join('cargos', 'cargos.CargArea', '=', 'areas.ID_Area')
-                ->join('personals', 'personals.FK_PersCargo', '=', 'cargos.ID_Carg')
-                ->join('users', 'users.FK_UserPers', 'personals.ID_Pers')
-                ->select('users.email', 'clientes.CliName', 'solicitud_servicios.*')
-                ->where('users.UsRol', 'JefeLogistica')//rol que va ha ser contactado para las solicitudes de servicio
-                ->first();
-            // $UsersMails = DB::table('users')
-            // $Roles1 = DB::table('users')
-            //     ->where('users.UsRol', 'JefeLogistica')
-            //     ->select('users.email')
-            //     ->get();
-            // $Roles2 = DB::table('users')
-            //     ->where('users.UsRol2', 'JefeLogistica')
-            //     ->select('users.email')
-            //     ->get();
-            //     foreach($Roles1 as $Rol1){
-            //         Mail::to($Rol1->email)->send(new SolSerEmail($email));
-            //     }
-            //     foreach($Roles2 as $Rol2){
-            //         Mail::to($Rol2->email)->send(new SolSerEmail($email));
-            //     }
-                // foreach($UsersMails as $UserMail){
-                    // Mail::to($UserMail->email)->send(new SolSerEmail($email));
-                    // }
+           
+            $Roles1 = DB::table('users')
+                ->whereIn('users.UsRol', ['JefeLogistica', 'AsistenteLogistica'])
+                ->select('users.email')
+                ->get();
+                
+            $Roles2 = DB::table('users')
+                ->whereIn('users.UsRol2', ['JefeLogistica', 'AsistenteLogistica'])
+                ->select('users.email')
+                ->get();
 
-
-                Mail::to($emailUser->email)->send(new SolSerEmail($email));
+                foreach($Roles1 as $Rol1){
+                    Mail::to($Rol1->email)->send(new SolSerEmail($email));
+                }
+                foreach($Roles2 as $Rol2){
+                    Mail::to($Rol2->email)->send(new SolSerEmail($email));
+                }
 
         }elseif($SolSer->SolSerStatus === 'Programado'){
             $email = DB::table('solicitud_servicios')
