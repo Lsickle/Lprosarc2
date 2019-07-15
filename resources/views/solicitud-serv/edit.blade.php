f3b28b5e6f8213640a6d9e0225eb29846c557df2
LoquilloDuvan <programmerloquillo@gmail.com>
2019-07-15 10:55:38 -0500

    Arreglando submit en el edit de la solicitud de servicio

--- resources/views/solicitud-serv/edit.blade.php
+++ resources/views/solicitud-serv/edit.blade.php
@@ -67,25 +67,23 @@
 									<small class="help-block with-errors">*</small>
 									<input maxlength="255" type="text" class="form-control" id="SolSerAdressTrans" name="SolSerAdressTrans" value="">
 								</div>
-								<div id="citytransportadora" class="form-group col-md-12" style="margin: 0; padding: 0;">
-									<div class="form-group col-md-6">
-										<label for="departamento">{{ trans('adminlte_lang::message.solsertransdepart') }}</label>
-										<select class="form-control select" id="departamento">
-											<option value="">{{ trans('adminlte_lang::message.select') }}</option>
-											@foreach ($Departamentos as $Departament)
-												<option value="{{$Departament->ID_Depart}}" {{ $Departamento->ID_Depart == $Departament->ID_Depart ? 'selected' : '' }}>{{$Departament->DepartName}}</option>
-											@endforeach
-										</select>
-									</div>
-									<div class="form-group col-md-6">
-										<label for="municipio">{{ trans('adminlte_lang::message.solsertransmuni') }}</label><a class="load"></a>
-										<small class="help-block with-errors">*</small>
-										<select name="SolSerCityTrans" class="form-control select" id="municipio">
-											@foreach($Municipios as $Municipi)
-												<option value="{{$Municipi->ID_Mun}}" {{ $Solicitud->SolSerCityTrans == $Municipi->MunName ? 'selected' : '' }}>{{$Municipi->MunName}}</option>
-											@endforeach
-										</select>
-									</div>
+								<div class="form-group col-md-6 citytransportadora">
+									<label for="departamento">{{ trans('adminlte_lang::message.solsertransdepart') }}</label>
+									<select class="form-control select" id="departamento">
+										<option value="">{{ trans('adminlte_lang::message.select') }}</option>
+										@foreach ($Departamentos as $Departament)
+											<option value="{{$Departament->ID_Depart}}" {{ $Departamento->ID_Depart == $Departament->ID_Depart ? 'selected' : '' }}>{{$Departament->DepartName}}</option>
+										@endforeach
+									</select>
+								</div>
+								<div class="form-group col-md-6 citytransportadora">
+									<label for="municipio">{{ trans('adminlte_lang::message.solsertransmuni') }}</label><a class="load"></a>
+									<small class="help-block with-errors">*</small>
+									<select name="SolSerCityTrans" class="form-control select" id="municipio">
+										@foreach($Municipios as $Municipi)
+											<option value="{{$Municipi->ID_Mun}}" {{ $Solicitud->SolSerCityTrans == $Municipi->MunName ? 'selected' : '' }}>{{$Municipi->MunName}}</option>
+										@endforeach
+									</select>
 								</div>
 								<div id="Conductor" class="form-group col-md-6">
 									<label for="SolSerConductor">{{ trans('adminlte_lang::message.solserconduc') }}</label>
@@ -234,7 +232,7 @@
 		$("#SolSerAdressTrans").attr('required', true);
 		$('#addresstransportadora').attr('hidden',false);
 		$("#municipio").attr('required', true);
-		$('#citytransportadora').attr('hidden',false);
+		$('.citytransportadora').attr('hidden',false);
 	@else
 		$("#SolSerNameTrans").attr('required', false);
 		$('#nametransportadora').attr('hidden',true);
@@ -243,7 +241,7 @@
 		$("#SolSerAdressTrans").attr('required', false);
 		$('#addresstransportadora').attr('hidden',true);
 		$("#municipio").attr('required', false);
-		$('#citytransportadora').attr('hidden',true);
+		$('.citytransportadora').attr('hidden',true);
 	@endif
 	$('#SolSerConductor').val('{{$Solicitud->SolSerConductor}}');
 	$('#Conductor').attr('hidden',false);
@@ -277,7 +275,7 @@
 	$("#SolSerAdressTrans").attr('required', false);
 	$('#addresstransportadora').attr('hidden',true);
 	$("#municipio").attr('required', false);
-	$('#citytransportadora').attr('hidden',true);
+	$('.citytransportadora').attr('hidden',true);
 	$('#Conductor').attr('hidden',true);
 	$('#Vehiculo').attr('hidden',true);
 	$('#typeaditable').removeClass('col-md-12');
@@ -313,307 +311,6 @@
 	$('#SolSerVehicExclusive').bootstrapSwitch('disabled',false);
 	$('#SolSerPlatform').bootstrapSwitch('disabled',false);
 @endif
-function TransportadorProsarc() {
-	$("#transportador").attr('hidden', true);
-	$("#transportador option:selected").prop("selected", false);
-	$("#Conductor").attr('hidden', true);
-	$("#SolSerConductor").val(null);
-	$("#Vehiculo").attr('hidden', true);
-	$("#SolSerVehiculo").val(null);
-	$("#typeaditable").removeClass('col-md-12');
-	$("#typeaditable").addClass('col-md-6');
-	$("#SolSerBascula").bootstrapSwitch('disabled',false);
-	$("#SolSerCapacitacion").bootstrapSwitch('disabled',false);
-	$("#SolSerMasPerson").bootstrapSwitch('disabled',false);
-	$("#SolSerVehicExclusive").bootstrapSwitch('disabled',false);
-	$("#SolSerPlatform").bootstrapSwitch('disabled',false);
-	$("#SolSerDevolucion").bootstrapSwitch('disabled',false);
-	$("#SolSerTransportador").removeAttr('required');
-	$("#typecollect").attr('hidden', false);
-	$("#SolSerTypeCollect").attr('required', true);
-	$("#typecollect").removeClass('col-md-6');
-	$("#typecollect").addClass('col-md-12');
-	$("#typecollect option:selected").prop("selected", false);
-	HiddenTypeCollect();
-	TransportadorCliente();
-}
-function HiddenTypeCollect(){
-	$("#sedecollect").attr('hidden', true);
-	$("#SedeCollect").attr('required', false);
-	$("#SedeCollect").val(null).trigger('change');
-	$("#addresscollect").attr('hidden', true);
-	$("#AddressCollect").attr('required', false);
-	$("#AddressCollect").val(null);
-	$("#typecollect").removeClass('col-md-6');
-	$("#typecollect").addClass('col-md-12');
-}
-function TypeCollectSede(){
-	$("#sedecollect").attr('hidden', false);
-	$("#SedeCollect").attr('required', true);
-	$("#addresscollect").attr('hidden', true);
-	$("#AddressCollect").attr('required', false);
-	$("#AddressCollect").val(null);
-	$("#typecollect").removeClass('col-md-12');
-	$("#typecollect").addClass('col-md-6');
-}
-function TypeCollectOther(){
-	$("#sedecollect").attr('hidden', true);
-	$("#SedeCollect").attr('required', false);
-	$("#SedeCollect").val(null).trigger('change');
-	$("#addresscollect").attr('hidden', false);
-	$("#AddressCollect").attr('required', true);
-	$("#AddressCollect").val(null);
-	$("#typecollect").removeClass('col-md-12');
-	$("#typecollect").addClass('col-md-6');
-}
-function TransportadorExtr() {
-	$("#transportador").attr('hidden', false);
-	$("#Conductor").attr('hidden', false);
-	$("#Vehiculo").attr('hidden', false);
-	$("#SolSerTransportador").attr('required', true);
-	$("#typeaditable").removeClass('col-md-6');
-	$("#typeaditable").addClass('col-md-12');
-	$("#SolSerBascula").bootstrapSwitch('state',false);
-	$("#SolSerBascula").bootstrapSwitch('disabled',true);
-	$("#SolSerCapacitacion").bootstrapSwitch('state',false);
-	$("#SolSerCapacitacion").bootstrapSwitch('disabled',true);
-	$("#SolSerMasPerson").bootstrapSwitch('state',false);
-	$("#SolSerMasPerson").bootstrapSwitch('disabled',true);
-	$("#SolSerVehicExclusive").bootstrapSwitch('state',false);
-	$("#SolSerVehicExclusive").bootstrapSwitch('disabled',true);
-	$("#SolSerPlatform").bootstrapSwitch('state',false);
-	$("#SolSerPlatform").bootstrapSwitch('disabled',true);
-	$("#SolSerDevolucion").bootstrapSwitch('disabled',false);
-	$("#typecollect").attr('hidden', true);
-	$("#SolSerTypeCollect").attr('required', false);
-	$("#typecollect option:selected").prop("selected", false);
-	HiddenTypeCollect();
-	TransportadorCliente();
-}
-
-function TransportadorCliente() {
-	$("#nametransportadora").attr('hidden', true);
-	$("#nittransportadora").attr('hidden', true);
-	$("#addresstransportadora").attr('hidden', true);
-	$("#citytransportadora").attr('hidden', true);
-	$("#SolSerNameTrans").removeAttr('required');
-	$("#SolSerNitTrans").removeAttr('required');
-	$("#SolSerAdressTrans").removeAttr('required');
-	$("#municipio").removeAttr('required');
-	$("#SolSerNameTrans").val(null);
-	$("#SolSerNitTrans").val(null);
-	$("#SolSerAdressTrans").val(null);
-	$("#municipio").empty();
-	$("#departamento").val(null).trigger('change');
-}
-
-function OtraTransportadora() {
-	$("#nametransportadora").attr('hidden', false);
-	$("#nittransportadora").attr('hidden', false);
-	$("#addresstransportadora").attr('hidden', false);
-	$("#citytransportadora").attr('hidden', false);
-	$("#SolSerNameTrans").attr('required', true);
-	$("#SolSerNitTrans").attr('required', true);
-	$("#SolSerAdressTrans").attr('required', true);
-	$("#municipio").attr('required', true);
-}
-var contadorGenerador = 1;
-var contadorRespel = [];
-function HiddenResiduosGener(id_div){
-	icon = $('button[data-target=".Respel'+id_div+'"]').find('svg');
-	$(icon).removeClass('fa-minus');
-	$(icon).addClass('fa-plus');
-	$("#DivRepel"+id_div).empty();
-}
-$("#SolSerDevolucion").on('switchChange.bootstrapSwitch', function(event, state) {
-	if(state == true){
-		$("#SolSerDevolucionTipo").parent().parent().attr('hidden', false);
-		$("#SolSerDevolucionTipo").attr('disabled', false);
-		$("#SolSerDevolucionTipo").attr('required', true);
-	}
-	else{
-		$("#SolSerDevolucionTipo").parent().parent().attr('hidden', true);
-		$("#SolSerDevolucionTipo").attr('disabled', true);
-		$("#SolSerDevolucionTipo").attr('required', false);
-		$("#SolSerDevolucionTipo").val(null);
-	}
-});
-function ResiduosGener(id_div, ID_Gener){
-	contadorRespel[id_div] = 0;
-	$("#DivRepel"+id_div).empty();
-	$("#DivRepel"+id_div).append(`@include('solicitud-serv.layaoutsSolSer.OneRespel')`);
-	$('#EditSolSer').validator('update');
-	Switch2();
-	Switch3();
-	Checkboxs();
-	numeroDimension();
-	numeroKg();
-	popover();
-	ChangeSelect();
-	Selects();
-	icon = $('button[data-target=".Respel'+id_div+'"]').find('svg');
-	$(icon).removeClass('fa-plus');
-	$(icon).addClass('fa-minus');
-	HiddenRequeRespel(id_div, contadorRespel[id_div]);
-	$.ajaxSetup({
-		headers: {
-			'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-		}
-	});
-	$.ajax({
-		url: "{{url('/RespelGener')}}/"+ID_Gener,
-		method: 'GET',
-		data:{},
-		success: function(res){
-			if(res != ''){
-				var residuos = new Array();
-				$("#FK_SolResRg"+id_div+contadorRespel[id_div]).empty();
-				$("#FK_SolResRg"+id_div+contadorRespel[id_div]).append(`<option onclick="HiddenRequeRespel(`+id_div+`,`+contadorRespel[id_div]+`)" value="">{{ trans('adminlte_lang::message.select') }}</option>`);
-				for(var i = res.length -1; i >= 0; i--){
-					if ($.inArray(res[i].SlugSGenerRes, residuos) < 0) {
-						$("#FK_SolResRg"+id_div+contadorRespel[id_div]).append(`<option onclick="RequeRespel(`+id_div+`,`+contadorRespel[id_div]+`,'`+res[i].RespelSlug+`')" value="${res[i].SlugSGenerRes}">${res[i].RespelName}</option>`);
-						residuos.push(res[i].SlugSGenerRes);
-					}
-				}
-			}
-			else{
-				$("#DivRepel"+id_div).empty();
-				NotifiFalse("Lo sentimos esta sede de generador no tiene residuos asignados");
-			}
-		},
-		error: function (jqXHR, textStatus, errorThrown) {
-			NotifiFalse("No se pudo conectar a la base de datos");
-		}
-	})
-}
-function RequeRespel(id_div, contador, Id_Respel){
-	$.ajaxSetup({
-		headers: {
-			'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-		}
-	});
-	$.ajax({
-		url: "{{url('/RequeRespel')}}/"+Id_Respel,
-		method: 'GET',
-		data:{},
-		success: function(res){
-			if(res[0] != ''){
-				if(res[0].ReqFotoDescargue === 1){
-					$('#SolResFotoDescargue_Pesaje'+id_div+contador).bootstrapSwitch('state',false);
-					$('#SolResFotoDescargue_Pesaje'+id_div+contador).bootstrapSwitch('disabled',false);
-				}
-				else{
-					$('#SolResFotoDescargue_Pesaje'+id_div+contador).bootstrapSwitch('state',false);
-					$('#SolResFotoDescargue_Pesaje'+id_div+contador).bootstrapSwitch('disabled',true);
-				}
-				if(res[0].ReqFotoDestruccion === 1){
-					$('#SolResFotoTratamiento'+id_div+contador).bootstrapSwitch('state',false);
-					$('#SolResFotoTratamiento'+id_div+contador).bootstrapSwitch('disabled',false);
-				}
-				else{
-					$('#SolResFotoTratamiento'+id_div+contador).bootstrapSwitch('state',false);
-					$('#SolResFotoTratamiento'+id_div+contador).bootstrapSwitch('disabled',true);
-				}
-				if(res[0].ReqVideoDescargue === 1){
-					$('#SolResVideoDescargue_Pesaje'+id_div+contador).bootstrapSwitch('state',false);
-					$('#SolResVideoDescargue_Pesaje'+id_div+contador).bootstrapSwitch('disabled',false);
-				}
-				else{
-					$('#SolResVideoDescargue_Pesaje'+id_div+contador).bootstrapSwitch('state',false);
-					$('#SolResVideoDescargue_Pesaje'+id_div+contador).bootstrapSwitch('disabled',true);
-				}
-				if(res[0].ReqVideoDestruccion === 1){
-					$('#SolResVideoTratamiento'+id_div+contador).bootstrapSwitch('state',false);
-					$('#SolResVideoTratamiento'+id_div+contador).bootstrapSwitch('disabled',false);
-				}
-				else{
-					$('#SolResVideoTratamiento'+id_div+contador).bootstrapSwitch('state',false);
-					$('#SolResVideoTratamiento'+id_div+contador).bootstrapSwitch('disabled',true);
-				}
-			}
-			else{
-				HiddenRequeRespel(id_div, contador);
-			}
-		},
-		error: function (jqXHR, textStatus, errorThrown) {
-			HiddenRequeRespel(id_div, contador);
-		}
-	});
-}
-function HiddenRequeRespel(id_div, contador){
-	$('#SolResFotoDescargue_Pesaje'+id_div+contador).bootstrapSwitch('state',false);
-	$('#SolResFotoDescargue_Pesaje'+id_div+contador).bootstrapSwitch('disabled',true);
-	$('#SolResFotoTratamiento'+id_div+contador).bootstrapSwitch('state',false);
-	$('#SolResFotoTratamiento'+id_div+contador).bootstrapSwitch('disabled',true);
-	$('#SolResVideoDescargue_Pesaje'+id_div+contador).bootstrapSwitch('state',false);
-	$('#SolResVideoDescargue_Pesaje'+id_div+contador).bootstrapSwitch('disabled',true);
-	$('#SolResVideoTratamiento'+id_div+contador).bootstrapSwitch('state',false);
-	$('#SolResVideoTratamiento'+id_div+contador).bootstrapSwitch('disabled',true);
-}
-function AgregarGenerador() {
-	$("#AddGenerador").before(`@include('solicitud-serv.layaoutsSolSer.NewGener')`);
-	$('#EditSolSer').validator('update');
-	popover();
-	ChangeSelect();
-	Selects();
-	contadorGenerador = contadorGenerador + 1;
-}
-
-function AgregarResPel(id_div,ID_Gener) {
-	contadorRespel[id_div] = contadorRespel[id_div]+1;
-	$("#AddRespel"+id_div).before(`@include('solicitud-serv.layaoutsSolSer.NewRespel')`);
-	Switch2();
-	Switch3();
-	Checkboxs();
-	numeroDimension();
-	numeroKg();
-	popover();
-	ChangeSelect();
-	Selects();
-	HiddenRequeRespel(id_div, contadorRespel[id_div]);
-	$.ajaxSetup({
-		headers: {
-			'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-		}
-	});
-	$.ajax({
-		url: "{{url('/RespelGener')}}/"+ID_Gener,
-		method: 'GET',
-		data:{},
-		success: function(res){
-			if(res != ''){
-				var residuos = new Array();
-				$("#FK_SolResRg"+id_div+contadorRespel[id_div]).empty();
-				$("#FK_SolResRg"+id_div+contadorRespel[id_div]).append(`<option onclick="HiddenRequeRespel(`+id_div+`,`+contadorRespel[id_div]+`)" value="">{{ trans('adminlte_lang::message.select') }}</option>`);
-				for(var i = res.length -1; i >= 0; i--){
-					if ($.inArray(res[i].SlugSGenerRes, residuos) < 0) {
-						$("#FK_SolResRg"+id_div+contadorRespel[id_div]).append(`<option onclick="RequeRespel(`+id_div+`,`+contadorRespel[id_div]+`,'`+res[i].RespelSlug+`')" value="${res[i].SlugSGenerRes}">${res[i].RespelName}</option>`);
-						residuos.push(res[i].SlugSGenerRes);
-					}
-				}
-			}
-			else{
-				$("#DivRepel"+id_div).empty();
-				NotifiFalse("Lo sentimos esta sede de generador no tiene residuos asignados");
-			}
-		},
-		error: function (jqXHR, textStatus, errorThrown) {
-			NotifiFalse("No se pudo conectar a la base de datos");
-		}
-	})
-	$('#EditSolSer').validator('update');
-}
-function RemoveRespel(id_div, contador) {
-	$("#Repel"+id_div+contador).prev().remove();
-	$("#Repel"+id_div+contador).remove();
-	$('#EditSolSer').validator('update');
-}
-
-function RemoveGenerador(id) {
-	$("#Generador"+id).prev().remove();
-	$("#Generador"+id).remove();
-	$('#EditSolSer').validator('update');
-}
-
 </script>
+@include('solicitud-serv.layaoutsSolSer.functionsSolSer')
 @endsection
