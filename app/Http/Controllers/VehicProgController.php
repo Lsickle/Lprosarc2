53623998a8d84a0fd5bfa73ac57b8784c409689f
LoquilloDuvan <programmerloquillo@gmail.com>
2019-07-23 16:59:09 -0500

    Se agrega ayudante en un servicio alquilado en el create de la programacion del servicio

--- app/Http/Controllers/VehicProgController.php
+++ app/Http/Controllers/VehicProgController.php
@@ -142,6 +142,7 @@ class VehicProgController extends Controller
 			else{
 				$programacion->ProgVehtipo = 2;
 				$programacion->FK_ProgVehiculo = $request->input('vehicalqui');
+				$programacion->FK_ProgAyudante = $request->input('FK_ProgAyudante');
 				$programacion->ProgVehColor = '#FFFF00';
 				$vehiculo = Vehiculo::select('VehicPlaca')->where('ID_Vehic', $request->input('vehicalqui'))->first()->VehicPlaca;
 				$nomConduct = null;
