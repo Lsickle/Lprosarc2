7683c5b9826e9d1c86f44d726765196d689d8031
LoquilloDuvan <programmerloquillo@gmail.com>
2019-07-26 10:40:55 -0500

    Se agregan textos al archivo messages del edit de los contratos

--- resources/views/contratos/edit.blade.php
+++ resources/views/contratos/edit.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.app')
 @section('htmlheader_title')
-{{-- {{ trans('adminlte_lang::message.cargotitle') }} --}}
-Contratos
+{{ trans('adminlte_lang::message.contracttitle') }}
 @endsection
 @section('contentheader_title')
-{{-- {{ trans('adminlte_lang::message.cargotitle') }} --}}
-Contratos
+{{ trans('adminlte_lang::message.contracttitle') }}
 @endsection
 @section('main-content')
 <div class="container-fluid spark-screen">
@@ -21,7 +19,7 @@ Contratos
 			@endcomponent
 			<div class="box">
 				<div class="box-header with-border">
-					<h3 class="box-title">{{-- {{trans('adminlte_lang::message.createcargo')}} --}}Edición de Contratos</h3>
+					<h3 class="box-title">{{trans('adminlte_lang::message.contractedit')}}</h3>
 					@if($Contrato->ContraDelete == 0)
 						<a method='get' href='#' data-toggle='modal' data-target='#myModal{{$Contrato->ContraSlug}}' class='btn btn-danger pull-right'><i class="fas fa-trash-alt"></i><b> {{ trans('adminlte_lang::message.delete') }}</b></a>
 						<form action='/contratos/{{$Contrato->ContraSlug}}' method='POST'>
@@ -43,7 +41,7 @@ Contratos
 						@csrf
 						<div class="box-body">
 							<div class="form-group col-md-6">
-								<label data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" data-delay='{"show": 500}' title="{{ trans('adminlte_lang::message.cargoareatittle') }}" data-content="{{ trans('adminlte_lang::message.cargoareainfo') }}" for="AreaSelect"><i style="font-size: 1.8rem; color: Dodgerblue;" class="fas fa-info-circle fa-2x fa-spin"></i>{{-- {{trans('adminlte_lang::message.inputarea')}} --}}Clientes</label><small class="help-block with-errors">*</small>
+								<label data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" data-delay='{"show": 500}' title="{{ trans('adminlte_lang::message.contractclien') }}" data-content="{{ trans('adminlte_lang::message.contractcliendescript') }}" for="AreaSelect"><i style="font-size: 1.8rem; color: Dodgerblue;" class="fas fa-info-circle fa-2x fa-spin"></i>{{trans('adminlte_lang::message.contractclien')}}</label><small class="help-block with-errors">*</small>
 								<select name="Fk_ContraCli" required id="Fk_ContraCli" class="form-control select">
 									<option value="">{{trans('adminlte_lang::message.select')}}</option>
 									@foreach($Clientes as $Cliente)
@@ -52,7 +50,7 @@ Contratos
 								</select>
 							</div>
 							<div class="form-group col-md-6">
-								<label data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" data-delay='{"show": 500}' title="{{ trans('adminlte_lang::message.cargogradetittle') }}" data-content="{{ trans('adminlte_lang::message.cargogradeinfo') }}" for="CargoGrade"><i style="font-size: 1.8rem; color: Dodgerblue;" class="fas fa-info-circle fa-2x fa-spin"></i>{{-- {{trans('adminlte_lang::message.cargograde')}} --}}Contrato</label>
+								<label data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" data-delay='{"show": 500}' title="{{ trans('adminlte_lang::message.contractpdf') }}" data-content="{{ trans('adminlte_lang::message.contractpdfdescript') }}" for="CargoGrade"><i style="font-size: 1.8rem; color: Dodgerblue;" class="fas fa-info-circle fa-2x fa-spin"></i>{{trans('adminlte_lang::message.contractpdf')}}</label>
 								<div class="input-group">
 									<input type="file" name="ContraPdf" data-validate="true" data-filesize="5120" class="form-control" data-accept="pdf" accept=".pdf">
 									<div class="input-group-btn">
@@ -61,12 +59,12 @@ Contratos
 								</div>
 							</div>
 							<div class="form-group col-md-6">
-								<label data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" data-delay='{"show": 500}' title="{{ trans('adminlte_lang::message.cargonametittle') }}" data-content="{{ trans('adminlte_lang::message.cargonameinfo') }}" for="NombreCargo"><i style="font-size: 1.8rem; color: Dodgerblue;" class="fas fa-info-circle fa-2x fa-spin"></i>{{-- {{trans('adminlte_lang::message.cargoname')}} --}}Fecha de vencimiento</label><small class="help-block with-errors">*</small>
+								<label data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" data-delay='{"show": 500}' title="{{ trans('adminlte_lang::message.contractvigencia') }}" data-content="{{ trans('adminlte_lang::message.contractvigenciadescript') }}" for="NombreCargo"><i style="font-size: 1.8rem; color: Dodgerblue;" class="fas fa-info-circle fa-2x fa-spin"></i>{{trans('adminlte_lang::message.contractvigencia')}}</label><small class="help-block with-errors">*</small>
 								<input required name="ContraVigencia" autofocus="true" type="date" class="form-control" id="ContraVigencia" value="{{$Contrato->ContraVigencia}}">
 							</div>
 							
 							<div class="form-group col-md-6">
-								<label data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" data-delay='{"show": 500}' title="{{ trans('adminlte_lang::message.cargosalarytittle') }}" data-content="{{ trans('adminlte_lang::message.cargosalaryinfo') }}" for="CargoSalary"><i style="font-size: 1.8rem; color: Dodgerblue;" class="fas fa-info-circle fa-2x fa-spin"></i>{{-- {{trans('adminlte_lang::message.cargosalary')}} --}}¿Cuando notificar?</label>
+								<label data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" data-delay='{"show": 500}' title="{{ trans('adminlte_lang::message.contractnotify') }}" data-content="{{ trans('adminlte_lang::message.contractnotifydescript') }}" for="CargoSalary"><i style="font-size: 1.8rem; color: Dodgerblue;" class="fas fa-info-circle fa-2x fa-spin"></i>{{trans('adminlte_lang::message.contractnotify')}}</label>
 								<div class="input-group">
 									<input type="text" class="form-control" name="ContratoNumVigencia"value="{{$Contrato->ContratoNumVigencia}}">
 									<input type="text" name="ContratoTypeVigencia" id="ContratoTypeVigencia" hidden="" value="{{$Contrato->COntratoTypeVigencia}}">
