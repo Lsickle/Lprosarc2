76b48d64f335ee363176636bae105de57a865139
LoquilloDuvan <programmerloquillo@gmail.com>
2019-07-29 10:49:37 -0500

    Se agrega validacion de cantidad en kg en el create de la solicitud de servicio

--- resources/views/solicitud-serv/create.blade.php
+++ resources/views/solicitud-serv/create.blade.php
@@ -14,7 +14,7 @@
 					<h3 class="box-title">{{ trans('adminlte_lang::message.solsertitlecreate') }}</h3>
 				</div>
 				<div class="box box-info">
-					<form role="form" id="CreateSolSer" action="/solicitud-servicio" method="POST" data-toggle="validator">
+					<form role="form" id="CreateSolSer" action="/solicitud-servicio" method="POST" enctype="multipart/form-data" data-toggle="validator">
 						@csrf
 						@if ($errors->any())
 							<div class="alert alert-danger" role="alert">
@@ -227,9 +227,11 @@
 								<a onclick="AgregarGenerador()" id="Agregar" class="btn btn-primary" data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" title="<b> {{ trans('adminlte_lang::message.solseraddgener') }}</b>" data-content="{{ trans('adminlte_lang::message.solseraddgenerdescrit') }}"><i class="fas fa-plus-circle"></i> {{ trans('adminlte_lang::message.solseraddgener') }}</a>
 							</div>
 						</div>
+						<div id="ModalSupport"></div>
 						<div class="box box-info">
 							<div class="box-footer">
-								<button type="submit" class="btn btn-success pull-right" form="CreateSolSer">{{ trans('adminlte_lang::message.applyfor') }}</button>
+								<a href="#" onclick="$('#Submit').hasClass('disabled') ? $('#Submit').click() : submitverify()" id="Submit2" class="btn btn-success pull-right">{{ trans('adminlte_lang::message.applyfor') }}</a>
+								<button type="submit" id="Submit" style="display: none;"></button>
 							</div>
 						</div>
 					</form>
@@ -250,6 +252,58 @@ function Switch(){
 	$("#SolSerDevolucion").bootstrapSwitch('disabled',true);
 }
 Switch();
+function submitverify(){
+	var CantidadTotalkg = 0;
+	for (var i = 0; i < contadorGenerador; i++) {
+		for (var y = 0; y <= contadorRespel[i]; y++) {
+			if($("#SolResKgEnviado"+i+y).val() != null){
+				CantidadTotalkg = parseInt(CantidadTotalkg)+parseInt($("#SolResKgEnviado"+i+y).val());
+			}
+		}
+	}
+	if(CantidadTotalkg != 0){
+		if(CantidadTotalkg >= 500){
+			$("#Submit2").empty();
+			$("#Submit2").append(`<i class="fas fa-sync fa-spin"></i> Enviando...`);
+			$("#Submit2").attr('disabled', true);
+			$('#Submit').click();
+		}
+		else{
+			$('#ModalSupport').empty();
+			$('#ModalSupport').append(`
+				<div class="modal modal-default fade in" id="SupportPay" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+								<div style="font-size: 5em; color: #f39c12; text-align: center; margin: auto;">
+									<i class="fas fa-exclamation-triangle"></i>
+									<span style="font-size: 0.3em; color: black;"><p>Su solicitud es inferior a 500kg adjunte el soporte de pago</p></span>
+									<span style="font-size: 0.3em; color: black;"><p>Su solicitud es de <b>`+CantidadTotalkg+` kg</b></p></span>
+								</div>
+							</div>
+							<div class="modal-header">
+								<div class="form-group col-md-12">
+									<label  color: black; text-align: left;" data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" title="<b>{{ trans('adminlte_lang::message.solsersupportpay') }}</b>" data-content="{{ trans('adminlte_lang::message.solsersupportpaydescript') }}"><i style="font-size: 1.8rem; color: Dodgerblue;" class="fas fa-info-circle fa-2x fa-spin"></i>{{trans('adminlte_lang::message.solsersupportpay')}}</label>
+									<small class="help-block with-errors"></small>
+									<input name="SupportPay" type="file" data-filesize="5120" class="form-control" data-accept="pdf" accept=".pdf">
+								</div>
+							</div> 
+							<div class="modal-footer">
+								<button type="button" class="btn btn-danger pull-left" data-dismiss="modal">No, salir</button>
+								<label for="Submit" class='btn btn-success'>Enviar</label>
+							</div>
+						</div>
+					</div>
+				</div>
+			`);
+			popover();
+			$('#CreateSolSer').validator('update');
+			envsubmit();
+			$('#SupportPay').modal();
+		}
+	}
+}
 </script>
 @include('solicitud-serv.layaoutsSolSer.functionsSolSer')
 @endsection
