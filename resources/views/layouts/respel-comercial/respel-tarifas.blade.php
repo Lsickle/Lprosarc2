363da5d26eadb1a39cc39a652aafc0fc68d52c17
luis de la hoz <davidoy69@hotmail.com>
2019-08-01 10:14:53 -0500

    estilo personalizado para ampliar la longitud del slider

--- resources/views/layouts/respel-comercial/respel-tarifas.blade.php
+++ resources/views/layouts/respel-comercial/respel-tarifas.blade.php
@@ -1,12 +1,36 @@
 <div id="tarifa`+contador+`Container">
-	<div class="col-md-6">
-		<label>tarifa</label>
+	<div class="col-md-12">
 		<span id="ex18-label-2a" class="hidden">Example low value</span>
 		<span id="ex18-label-2b" class="hidden">Example high value</span>
-		<input id="ex18b" type="text"/>
+		<input class="form-control" id="ex18b" type="number"/>
 	</div>
-	<div class="col-md-6">
-		<label>rango</label>
+	<div class="col-md-2">
+		<label>rango 1</label>
+		<input name="RespelDescrip" type="text" class="form-control" placeholder="Descripcion del Residuo" value="{{$Respels->RespelDescrip}}" disabled>
+	</div>
+
+	<div class="col-md-2">
+		<label>rango 1</label>
+		<input name="RespelDescrip" type="text" class="form-control" placeholder="Descripcion del Residuo" value="{{$Respels->RespelDescrip}}" disabled>
+	</div>
+
+	<div class="col-md-2">
+		<label>rango 1</label>
+		<input name="RespelDescrip" type="text" class="form-control" placeholder="Descripcion del Residuo" value="{{$Respels->RespelDescrip}}" disabled>
+	</div>
+
+	<div class="col-md-2">
+		<label>rango 1</label>
+		<input name="RespelDescrip" type="text" class="form-control" placeholder="Descripcion del Residuo" value="{{$Respels->RespelDescrip}}" disabled>
+	</div>
+
+	<div class="col-md-2">
+		<label>rango 1</label>
+		<input name="RespelDescrip" type="text" class="form-control" placeholder="Descripcion del Residuo" value="{{$Respels->RespelDescrip}}" disabled>
+	</div>
+
+	<div class="col-md-2">
+		<label>rango 1</label>
 		<input name="RespelDescrip" type="text" class="form-control" placeholder="Descripcion del Residuo" value="{{$Respels->RespelDescrip}}" disabled>
 	</div>
 </div>
