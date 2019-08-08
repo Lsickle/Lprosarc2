a8e74f466c3af564960d982da8c0614de520a7ae
luis de la hoz <davidoy69@hotmail.com>
2019-08-08 17:09:14 -0500

    modificado el maquetado de las tarifas para incluir el vencimiento

--- resources/views/layouts/respel-comercial/respel-rango.blade.php
+++ resources/views/layouts/respel-comercial/respel-rango.blade.php
@@ -1,4 +1,4 @@
-<div class="col-md-4" id="rango`+opcion+``+last+`">
+<div class="col-md-3" id="rango`+opcion+``+last+`">
 	<label style="font-size: 0.8em;">Desde `+rango+`</label><a onclick="EliminarRango(`+opcion+`,`+last+`)" id="minusrangeButton`+contadorRango[contador]+`"><i style="color:red; margin: 0; padding: 0; margin-top: 0.25em; cursor: pointer;" class="fa fa-trash-alt pull-right"></i></a>
 	<input name="Opcion[`+opcion+`][TarifaPrecio][]" type="text" class="form-control">
 	<input name="Opcion[`+opcion+`][TarifaDesde][]" hidden value="`+rango+`">
