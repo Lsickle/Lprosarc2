3a75a646a5d340f15b62ebaaa01bc58b334977c7
LoquilloDuvan <programmerloquillo@gmail.com>
2019-08-09 10:10:40 -0500

    Se agrega el modelo de requerimientos del cliente

--- app/RequerimientosCliente.php
+++ app/RequerimientosCliente.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class RequerimientosCliente extends Model
 {
-    //
+	protected $table='requerimientos_clientes';
+
+	protected $fillable=['RequeCliBascula', 'RequeCliCapacitacion', 'RequeCliMasPerson', 'RequeCliVehicExclusive', 'RequeCliPlatform', 'FK_RequeClient'];
+
+	protected $primaryKey = 'ID_RequeCli';
+		/**
+	 * Get the route key for the model.
+	 *
+	 * @return string
+	 */
+	public function getRouteKeyName()
+	{
+		return 'ID_RequeCli';
+	}
+	public function clientes()
+	{
+	 return $this->belongsTo('App\Cliente','FK_RequeClient', 'ID_Cli');
+	}
 }
+ 
--- database/migrations/2019_08_09_095552_create_requerimientos_clientes_table.php
+++ database/migrations/2019_08_09_095552_create_requerimientos_clientes_table.php
@@ -6,36 +6,36 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateRequerimientosClientesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('requerimientos_clientes', function (Blueprint $table) {
-            $table->bigIncrements('ID_RequeCli');
-            $table->boolean('RequeCliBascula')->nullable();
-            $table->boolean('RequeCliCapacitacion')->nullable();
-            $table->boolean('RequeCliMasPerson')->nullable();
-            $table->boolean('RequeCliVehicExclusive')->nullable();
-            $table->boolean('RequeCliPlatform')->nullable();
-            $table->unsignedInteger('FK_RequeClient')->nullable();
-            $table->foreign('FK_RequeClient')->references('ID_Cli')->on('clientes')->onDelete('cascade');
-            $table->timestamps();
-            $table->engine = 'InnoDB';
-            $table->charset = 'utf8';
-            $table->collation = 'utf8_unicode_ci';
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('requerimientos_clientes', function (Blueprint $table) {
+			$table->bigIncrements('ID_RequeCli');
+			$table->boolean('RequeCliBascula')->nullable();
+			$table->boolean('RequeCliCapacitacion')->nullable();
+			$table->boolean('RequeCliMasPerson')->nullable();
+			$table->boolean('RequeCliVehicExclusive')->nullable();
+			$table->boolean('RequeCliPlatform')->nullable();
+			$table->unsignedInteger('FK_RequeClient')->nullable();
+			$table->foreign('FK_RequeClient')->references('ID_Cli')->on('clientes')->onDelete('cascade');
+			$table->timestamps();
+			$table->engine = 'InnoDB';
+			$table->charset = 'utf8';
+			$table->collation = 'utf8_unicode_ci';
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('requerimientos_clientes');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('requerimientos_clientes');
+	}
 }
