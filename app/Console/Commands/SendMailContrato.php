3f0ceed609a74995fc473be118f477fc6f6cedc9
Andres <andres13marmon@gmail.com>
2019-08-09 15:05:18 -0500

    Modificando envio de email

--- app/Console/Commands/SendMailContrato.php
+++ app/Console/Commands/SendMailContrato.php
@@ -43,9 +43,7 @@ class SendMailContrato extends Command
      */
     public function handle()
     {
-        // $User = User::where('UsRol', 'Tesorería')->Orwhere('UsRol2', 'Tesorería')->get();
-        $Users = User::select('email')->where('UsRol', 'Tesorería')->get();
-        $Users2 = User::select('email')->where('UsRol2', 'Tesorería')->get();
+        $Users = User::where('UsRol', 'Programador')->orwhere('UsRol2', 'Programador')->get();
         $Contratos = DB::table('contratos')
             ->join('clientes', 'clientes.ID_Cli', 'contratos.Fk_ContraCli')
             ->select('clientes.CliShortname', 'contratos.*')
@@ -55,11 +53,6 @@ class SendMailContrato extends Command
         foreach($Users as $User){
             if(isset($Contratos[0]->ContraNotifiVigencia)){
                 Mail::to($User->email)->send(new ContartoMail($Contratos));
-            }
-        }
-        foreach($Users2 as $User2){
-            if(isset($Contratos[0]->ContraNotifiVigencia)){
-                Mail::to($User2->email)->send(new ContartoMail($Contratos));
             }
         }
     }
