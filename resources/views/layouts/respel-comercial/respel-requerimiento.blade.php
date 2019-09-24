d941e90ab5ffc94c4b70b04571ef4514f1843d9d
luis de la hoz <davidoy69@hotmail.com>
2019-09-24 13:29:04 -0500

    fixed error en clase de tituulo de opcion para evaluacion de respel

--- resources/views/layouts/respel-comercial/respel-requerimiento.blade.php
+++ resources/views/layouts/respel-comercial/respel-requerimiento.blade.php
@@ -1,6 +1,6 @@
 <div id="requerimiento`+contador+`Container" class="panel panel-default" style="display: inline-block; overflow: hidden; width:100%; background-color:#FAFAFF;">
 		<div style="padding: 0.25em; background-color: #222d32; color: #b8c7ce" class="panel-heading">
-		  <h5 class="panel-title">Tratamiento:<b style="color: #E8E8E8" id="pretratamiento`+contador+`TratName"> Seleccione un Tratamiento...</b>{{-- 	<small>Subtext for header</small> --}}</h5>
+		  <h5 class="panel-title">Tratamiento:<b style="color: #E8E8E8" id="requerimiento`+contador+`TratName"> Seleccione un Tratamiento...</b>{{-- 	<small>Subtext for header</small> --}}</h5>
 		</div>
 		<div style="margin-top: 0.25em;">
 			<div class="col-md-2 col-xs-6">
