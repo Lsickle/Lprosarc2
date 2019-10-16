74ab88e54e713da817d6efee87b91a7333a31c87
luis de la hoz <davidoy69@hotmail.com>
2019-10-16 14:22:08 -0500

    optimizada la pagina de verificacion de correo electronico

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -8,18 +8,31 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="box">
-                    <div class="box-header with-border">
+                <div class="box-header">
                         <h3 class="box-title">Verifica tu correo electrónico</h3>
-
+                </div>
                 <div class="box-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            Se ha enviado un nuevo enlace de verificación a su correo electrónico.
+                    <div class="row">
+                        <div class="col-md-12">
+                            @if (session('resent'))
+                            <div class="alert alert-success" role="alert">
+                                Se ha enviado un nuevo enlace de verificación a su correo electrónico.
+                            </div>
+                            @endif
+                            Por favor, verifique su Cuenta con el enlace que le fue enviado a su correo electrónico.
+                            <br><br>
+                            Si no ha recibido el correo electrónico:
+                            <ul style="margin-left: 2rem;">
+                                <li>Revise en su correo la carpeta de "Correo no Deseado" o "SPAM".</li>
+                                <li>Revise que haya escrito correctamente su correo durante el registro.</li>
+                                <li>Haga click en el botón para solicitar otro.</li>
+                            </ul>
                         </div>
-                    @endif
-
-                    Antes de continuar, por favor, confirme su correo electrónico con el enlace de verificación que le fue enviado.
-                    Si no ha recibido el correo electrónico, haga <a href="{{ route('verification.resend') }}">clic aquí</a> para solicitar otro.
+                    </div>
+                </div>
+                <div class="box-footer">
+                    <a class=" pull-left btn btn-primary" href="/profile/{{Auth::user()->UsSlug}}/edit">Editar Correo</a>
+                    <a class=" pull-right btn btn-success" href="{{ route('verification.resend') }}">reenviar</a>
                 </div>
             </div>
         </div>
