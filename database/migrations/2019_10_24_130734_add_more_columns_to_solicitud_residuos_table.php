fc2be573b9edaac8625680efee2f658ad06c2cb3
luis de la hoz <davidoy69@hotmail.com>
2019-10-24 16:03:29 -0500

    se cambia el nombre de la migracion ya que estaba duplicado

--- database/migrations/2019_10_24_130734_add_more_columns_to_solicitud_residuos_table.php
+++ database/migrations/2019_10_24_130734_add_more_columns_to_solicitud_residuos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddSomeColumnsToSolicitudResiduosTable extends Migration
+class AddMoreColumnsToSolicitudResiduosTable extends Migration
 {
     /**
      * Run the migrations.
