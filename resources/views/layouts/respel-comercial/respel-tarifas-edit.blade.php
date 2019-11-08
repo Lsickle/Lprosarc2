d8ee38197d1511769a411f66da7de71839aec8a4
luis de la hoz <davidoy69@hotmail.com>
2019-11-08 09:58:58 -0500

    corregido estilos de la fecha de vencimiento para que se ajuste a la presentacion en tablets

--- resources/views/layouts/respel-comercial/respel-tarifas-edit.blade.php
+++ resources/views/layouts/respel-comercial/respel-tarifas-edit.blade.php
@@ -8,14 +8,14 @@
     	@endphp
 			@foreach($opcion->tarifas as $tarifa)
 				<div class=row>
-					<div class="col-md-4 col-sm-6" {{-- style="max-height: 2.3em;" --}}>
-						<label for="expireRange{{$contadorphp}}" style="font-size: 0.9em;">Vencimiento</label>
+					<div class="col-md-4 col-sm-6" style="max-height: 58.1px;">
+						<label for="expireRange{{$contadorphp}}" style="font-size: 0.9em; margin-bottom: 0px;">Vencimiento</label>
 						<input {{in_array(Auth::user()->UsRol, Permisos::COMERCIAL)||in_array(Auth::user()->UsRol2, Permisos::COMERCIAL) ? '' : 'disabled' }} id="expireRange{{$contadorphp}}" name="Opcion[{{$contadorphp}}][TarifaVencimiento]" type="date" class="form-control" value="{{$tarifa->TarifaVencimiento}}">
 						@if(in_array(Auth::user()->UsRol, Permisos::JefeOperaciones)||in_array(Auth::user()->UsRol2, Permisos::JefeOperaciones))
 					   		<input type="date" hidden name="Opcion[{{$contadorphp}}][TarifaVencimiento]" value="{{$tarifa->TarifaVencimiento}}">
 					    @endif
 					</div>
-					<div class="col-md-4 col-sm-6">
+					<div class="col-md-3 col-sm-6">
 						<label for="frecrangeSelect{{$contadorphp}}" style="font-size: 0.9em;" data-trigger="hover" data-toggle="popover" title="<b>Frecuencia</b>" data-content="<p> se tomara en cuenta para la aplicación de la tarifa respectiva y el calculo del precio según la frecuencia de la cantidad puesta en planta Prosarc S.A. ESP</p>" >Frec.</label>
 						<select {{in_array(Auth::user()->UsRol, Permisos::COMERCIAL)||in_array(Auth::user()->UsRol2, Permisos::COMERCIAL) ? '' : 'disabled' }} id="frecrangeSelect{{$contadorphp}}" name="Opcion[{{$contadorphp}}][TarifaFrecuencia]">
 							<option {{$tarifa->TarifaFrecuencia == 'N/A' ? "selected" : ""}}>N/A</option>
@@ -26,7 +26,7 @@
 					   		<input hidden name="Opcion[{{$contadorphp}}][TarifaFrecuencia]" value="{{$tarifa->TarifaFrecuencia}}">
 					    @endif
 					</div>
-					<div class="col-md-2 col-sm-6">
+					<div class="col-md-3 col-sm-6">
 						<label style="font-size: 0.9em;" for="typerangeSelect{{$contadorphp}}">Tipo</label>
 						<select {{in_array(Auth::user()->UsRol, Permisos::COMERCIAL)||in_array(Auth::user()->UsRol2, Permisos::COMERCIAL) ? '' : 'disabled' }} id="typerangeSelect{{$contadorphp}}" name="Opcion[{{$contadorphp}}][Tarifatipo]">
 							<option {{$tarifa->Tarifatipo == "Kg" ? "selected" : "" }} >Kg</option>
