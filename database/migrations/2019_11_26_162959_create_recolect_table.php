88f8c44591b4260e233865549bb545872cc792ea
luis de la hoz <davidoy69@hotmail.com>
2019-11-26 17:46:03 -0500

    trabajando en la migraciones para documentos

--- database/migrations/2019_11_26_162959_create_recolect_table.php
+++ database/migrations/2019_11_26_162959_create_recolect_table.php
@@ -15,6 +15,10 @@ class CreateRecolectTable extends Migration
     {
         Schema::create('recolect', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedInteger('FK_ColectSgen');
+            $table->unsignedInteger('FK_ColectProg');
+            $table->foreign('FK_ColectSgen')->references('ID_GSede')->on('ID_GSede');
+            $table->foreign('FK_ColectProg')->references('ID_ProgVeh')->on('progvehiculos');
             $table->timestamps();
         });
     }
