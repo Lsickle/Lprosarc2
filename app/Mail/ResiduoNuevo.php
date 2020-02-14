1d43152ead193fc5ebc96e3475fb523dccd4c217
luis de la hoz <davidoy69@hotmail.com>
2020-02-14 16:51:41 -0500

    testing mail for newrespel

---
+++ app/Mail/ResiduoNuevo.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ResiduoNuevo extends Mailable implements ShouldQueue
+{
+    use Queueable;
+
+    public $respel;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($respel)
+    {
+        $this->respel = $respel;                    
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('notificaciones@prosarc.com.co', 'Prosarc S.A. ESP')
+                    ->subject('Se ha registrado el residuo '.$this->respel->RespelName)
+                    ->markdown('emails.residuo.nuevo');
+    }
+}
---
+++ resources/views/emails/residuo/nuevo.blade.php
@@ -0,0 +1,33 @@
+{{-- @php
+    // $url = url("/respels/{$respel->RespelSlug}");
+    // $nameButton = 'Ver Residuo';
+@endphp --}}
+@component('mail::message')
+# Nuevo residuo registrado
+
+El residuo <b>{{$respel->RespelName}}</b> ha sido registrado por el Cliente <b>{{$respel['cliente']->CliName}} -> {{$respel['cliente']->SedeName}}</b> y debe ser evaluado por el área de Operaciones para asignar los tratamientos viables que correspondan.<br><br>
+
+Por favor revise la información  del residuo, usando el siguiente botón<br><br>
+
+@component('mail::button', ['url' => url('/respels', [$respel->RespelSlug])])
+{{-- {{$nameButton}} --}}
+Ver Residuo
+@endcomponent
+
+Si tiene alguna duda comuníquese con el asesor comercial {{$respel['comercial']->PersFirstName.' '.$respel['comercial']->PersLastName}}. al teléfono {{$respel['comercial']->PersCellphone}} o con el personal del cliente usando los siguientes datos:<br>
+<ul>
+    <li>Nombre: {{$respel['personalcliente']->PersFirstName.' '.$respel['personalcliente']->PersLastName}} </li>
+    <li>teléfono: {{$respel['personalcliente']->PersCellphone}}</li>
+    <li>correo: {{$respel['personalcliente']->PersEmail}}</li>
+</ul>
+Saludos, Prosarc S.A. ESP.
+{{-- @component('mail::subcopy')
+@lang(
+    "Si tiene problemas para hacer clic en el botón \":actionText\", copie y pegue la siguiente URL \nen su navegador web: [:actionURL](:actionURL)",
+    [
+        'actionText' => $nameButton,
+        'actionURL' => $url,
+    ]
+)
+@endcomponent --}}
+@endcomponent
