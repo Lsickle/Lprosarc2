c779474c3fbb9a5e2ea8bde22be211f842419ba9
luis de la hoz <davidoy69@hotmail.com>
2020-08-18 10:35:00 -0500

    se verifica que todos los residuos de un servicio tengan tratamiento ofertado durante la funcion de repetir servicio

--- app/Http/Controllers/SolicitudServicioController.php
+++ app/Http/Controllers/SolicitudServicioController.php
@@ -805,7 +805,12 @@ class SolicitudServicioController extends Controller
 			        ->where('FK_ReqRespel', '=', $respelgener->FK_Respel)
 			        ->where('ofertado', '=', 1)
 			        ->where('forevaluation', '=', 1)
-			        ->first();
+					->first();
+					
+				if ($requerimientoOfertado == null) {
+					$SolicitudNew->delete();
+					abort(404, 'el servicio no se puede repetir debido a que alguno de los residuos no posee tratamiento ofertado, Verifique con su asesor Comercial');
+				}
 				if ($requerimientoOfertado->ReqFotoDescargue==0) {
 					$SolResNew->SolResFotoDescargue_Pesaje = 0;
 				}else{
