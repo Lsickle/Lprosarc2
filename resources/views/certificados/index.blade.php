0946bd5f666c0a1284cb289c17b45e00dd06c6ec
luis de la hoz <davidoy69@hotmail.com>
2020-09-23 09:54:24 -0500

    se intercambia etiqueta th por td para mantener coherencia de sintaxis

--- resources/views/certificados/index.blade.php
+++ resources/views/certificados/index.blade.php
@@ -54,8 +54,8 @@ Lista de Certificados
 								<tr>
 									<td>{{date('Y/m/d', strtotime($certificado->recepcion))}}</td>
 									@if(in_array(Auth::user()->UsRol, Permisos::TODOPROSARC))
-									<td class="text-center">{{$certificado->CertNumRm}}</td>
-										<th class="text-center">{{$certificado->cliente}}</th>
+										<td class="text-center">{{$certificado->CertNumRm}}</td>
+										<td class="text-center"><b>{{$certificado->cliente}}</b></td>
 									@endif
 									<td class="text-center">#{{$certificado->FK_CertSolser}}</td>
 									<td class="text-center">{{$certificado->tratamiento->TratName}}</td>
