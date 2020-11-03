65f13080691000f1f6b193d351c8ffb6a011abba
luis de la hoz <davidoy69@hotmail.com>
2020-11-03 13:17:48 -0500

    se ajusta el peso del copyright

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -20,7 +20,7 @@
     {{-- Footer --}}
     @slot('footer')
         @component('mail::footer')
-            © {{ date('Y') }} {{ config('app.name') }}. @lang('Todos los derechos reservados.')
+            © {{ date('Y') }} {{ 'Prosarc S.A. ESP' }}. @lang('Todos los derechos reservados.')
         @endcomponent
     @endslot
 @endcomponent
