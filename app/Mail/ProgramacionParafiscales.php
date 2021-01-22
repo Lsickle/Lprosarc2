f7157db464f2471fa682918006e505c16d4b163f
luis de la hoz <davidoy69@hotmail.com>
2021-01-22 09:34:01 -0500

    se crea notificacion por correo para las modificaciones de residuos incompletos por parte del cliente

--- app/Mail/ProgramacionParafiscales.php
+++ app/Mail/ProgramacionParafiscales.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class ProgramacionParafiscales extends Mailable
+class ProgramacionParafiscales extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
